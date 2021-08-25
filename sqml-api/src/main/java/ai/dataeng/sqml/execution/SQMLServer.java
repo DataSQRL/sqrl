bcf0dae484a9810963f4896d9c92a09e1629bfcb
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2021-08-25 12:05:22 -0700

    Added SQMLServer

---
+++ sqml-api/src/main/java/ai/dataeng/sqml/execution/SQMLServer.java
@@ -0,0 +1,49 @@
+package ai.dataeng.sqml.execution;
+
+import ai.dataeng.sqml.db.keyvalue.HierarchyKeyValueStore;
+import ai.dataeng.sqml.flink.DefaultEnvironmentProvider;
+import ai.dataeng.sqml.flink.EnvironmentProvider;
+import ai.dataeng.sqml.ingest.DataSourceRegistry;
+import ai.dataeng.sqml.source.SourceDataset;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class SQMLServer {
+
+    private final HierarchyKeyValueStore.Factory metadataStoreFactory;
+    private final DataSourceRegistry registry;
+
+    private final EnvironmentProvider envProvider = new DefaultEnvironmentProvider();
+
+    public SQMLServer(HierarchyKeyValueStore.Factory metadataStoreFactory) {
+        this.metadataStoreFactory = metadataStoreFactory;
+        this.registry = new DataSourceRegistry(metadataStoreFactory);
+    }
+
+    public void addDataSet(SourceDataset dataset) {
+        registry.addDataset(dataset);
+    }
+
+    public void run(Bundle sqml) throws Exception {
+        StreamExecutionEnvironment flinkEnv = envProvider.get();
+        compile(sqml,flinkEnv);
+        //4. Store bundle meta data and execute Flink Job
+        flinkEnv.execute("somename");
+    }
+
+    public void compile(Bundle sqml, StreamExecutionEnvironment flinkEnv) {
+        //1. Parse
+
+        //2. Analyze: resolve import dependencies, data types, and view definitions
+
+        //3. Construct Flink Job
+    }
+
+    public void start() {
+        //Restore bundles from meta data
+        //Recover all Flink jobs from savepoint
+    }
+
+    public void shutdown() {
+        //Savepoint Flink Jobs and shut down gracefully
+    }
+}
