6e8e97e312720023f8c0939f0b0444648cf633ed
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2021-12-07 16:11:04 -0800

    Changed function handling in AggregateOperator

--- sqml-api/src/main/java/ai/dataeng/sqml/logical4/AggregateOperator.java
+++ sqml-api/src/main/java/ai/dataeng/sqml/logical4/AggregateOperator.java
@@ -1,30 +1,45 @@
 package ai.dataeng.sqml.logical4;
 
+import ai.dataeng.sqml.function.FunctionHandle;
 import ai.dataeng.sqml.relation.CallExpression;
-import ai.dataeng.sqml.relation.VariableReferenceExpression;
+
 import java.util.List;
 import java.util.Map;
+
+import ai.dataeng.sqml.relation.RowExpression;
+import ai.dataeng.sqml.schema2.Type;
 import lombok.Value;
+
 
 @Value
 public class AggregateOperator extends LogicalPlan.RowNode<LogicalPlan.RowNode> {
-    Map<VariableReferenceExpression, Aggregation> aggregations;
-    List<VariableReferenceExpression> groupingKeys;
 
-    public AggregateOperator(LogicalPlan.RowNode input, Map<VariableReferenceExpression, Aggregation> aggregations, List<VariableReferenceExpression> groupingKeys) {
+    final LogicalPlan.Column[] groupByKeys;
+    final Map<LogicalPlan.Column, Aggregation> aggregates;
+    final LogicalPlan.Column[] schema;
+
+    public AggregateOperator(LogicalPlan.RowNode input, LogicalPlan.Column[] groupByKeys, Map<LogicalPlan.Column, Aggregation> aggregates) {
         super(input);
-
-        this.aggregations = aggregations;
-        this.groupingKeys = groupingKeys;
+        this.groupByKeys = groupByKeys;
+        this.aggregates = aggregates;
+        schema = new LogicalPlan.Column[groupByKeys.length + aggregates.size()];
+        int offset = groupByKeys.length;
+        System.arraycopy(groupByKeys,0, schema, 0, offset);
+        for (LogicalPlan.Column col : aggregates.keySet()) {
+            schema[offset++] = col;
+        }
     }
 
     @Override
     public LogicalPlan.Column[][] getOutputSchema() {
-        return new LogicalPlan.Column[0][];
+        return new LogicalPlan.Column[][]{schema};
     }
 
     @Value
     public static class Aggregation {
-        CallExpression call;
+
+        FunctionHandle functionHandle;
+        List<LogicalPlan.Column> arguments;
+
     }
 }
