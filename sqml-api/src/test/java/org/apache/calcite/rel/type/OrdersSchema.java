aee4a7ade837933802078c1ff9d4a33a607e7f05
Daniel Henneberger <git@danielhenneberger.com>
2021-12-23 00:09:58 -0800

    Cleanup
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqml-api/src/test/java/org/apache/calcite/rel/type/OrdersSchema.java
+++ sqml-api/src/test/java/org/apache/calcite/rel/type/OrdersSchema.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.jdbc.TableResolver;
 import org.apache.calcite.linq4j.tree.Expression;
-import org.apache.calcite.schema.CustomColumnResolvingTable;
 import org.apache.calcite.schema.Function;
 import org.apache.calcite.schema.Schema;
 import org.apache.calcite.schema.SchemaPlus;
@@ -18,34 +17,15 @@ import org.apache.calcite.schema.Table;
 import org.apache.calcite.schema.impl.AbstractTable;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlNode;
-import org.apache.calcite.util.Pair;
 
 @AllArgsConstructor
 public class OrdersSchema implements Schema {
     TableResolver tableResolver;
 
-//  public static class SqrlTable implements CustomColumnResolvingTable {
   public static class SqrlTable extends AbstractTable {
-
-  private DynamicRecordTypeImpl2 set = null;
-
-//    private final List<LazyRelDataType> rels;
-
-    public SqrlTable() {
-//      this.rels = rels;
-    }
 
     @Override
     public RelDataType getRowType(RelDataTypeFactory relDataTypeFactory) {
-//      SqrlTypeFactory sqrlTypeFactory = (SqrlTypeFactory)relDataTypeFactory;
-      //      final RelDataTypeFactory.Builder fieldInfo = relDataTypeFactory.builder();
-//      //Cannot be ANY type as Field list is null
-//      //new LazyRelDataType(STRUCTURED, true, List.of(), relDataTypeFactory)
-//      relDataTypeFactory.createStructType(MockTable.this.getRowType().getFieldList());;
-//      fieldInfo.add("x", new UnknownRelSqlType(relDataTypeFactory, "?"));
-//      if (set == null) {
-//        this.set = new DynamicRecordTypeImpl2(relDataTypeFactory);
-//      }
       return
           relDataTypeFactory.createStructType(StructKind.PEEK_FIELDS, List.of(
           new DynamicRecordTypeImpl2(relDataTypeFactory),
@@ -74,53 +54,6 @@ public class OrdersSchema implements Schema {
         CalciteConnectionConfig calciteConnectionConfig) {
       return false;
     }
-//
-//    @Override
-//    public List<Pair<RelDataTypeField, List<String>>> resolveColumn(RelDataType relDataType,
-//        RelDataTypeFactory relDataTypeFactory, List<String> list) {
-//
-//      return List.of(Pair.of(
-//          new RelDataTypeFieldImpl("x", 0,
-//              new DynamicRecordTypeImpl2(relDataTypeFactory)),
-//          List.of() //consume all the tokens, we'll determine the type of this later
-//        )
-//      );
-//
-//      if (list.size() == 4) {
-//        return List.of(
-//            Pair.of(
-//                new RelDataTypeFieldImpl("orders", 0,
-//                    new LazyRelDataType(STRUCTURED, true, List.of(), relDataTypeFactory)),
-////                    relDataTypeFactory.createStructType(PEEK_FIELDS, List.of(), List.of())),
-//                list.subList(1, list.size())
-//            )
-//        );
-//      } else if (list.size() == 3) {
-//        return List.of(
-//            Pair.of(
-//                new RelDataTypeFieldImpl("entries", 0,
-//                    relDataTypeFactory.createStructType(PEEK_FIELDS, List.of(), List.of())),
-//                list.subList(1, list.size())
-//            )
-//        );
-//      }else if (list.size() == 2) {
-//        return List.of(
-//            Pair.of(
-//                new RelDataTypeFieldImpl("parent", 0,
-//                    relDataTypeFactory.createStructType(PEEK_FIELDS, List.of(), List.of())),
-//                list.subList(1, list.size())
-//            )
-//        );
-//      } else {
-//        return List.of(
-//            Pair.of(
-//                new RelDataTypeFieldImpl("b", 0,
-//                    relDataTypeFactory.createSqlType(ANY)),
-//                list.subList(1, list.size())
-//            )
-//        );
-//      }
-//    }/**/
   }
 
   @Override
