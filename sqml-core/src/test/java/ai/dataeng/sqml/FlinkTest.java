e1c0724b1a40e7c3093fb3f289791b5d9d4a06a6
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2022-02-20 10:35:05 -0800

    shredding in the table api

--- sqml-core/src/test/java/ai/dataeng/sqml/FlinkTest.java
+++ sqml-core/src/test/java/ai/dataeng/sqml/FlinkTest.java
@@ -5,6 +5,9 @@ import ai.dataeng.sqml.planner.operator.C360Test;
 import lombok.SneakyThrows;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.annotation.FunctionHint;
+import org.apache.flink.table.annotation.FunctionHints;
 import org.apache.flink.table.api.*;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.functions.TableFunction;
@@ -61,9 +64,9 @@ public class FlinkTest {
                         .column("time", DataTypes.INT())
                         .column("entries", DataTypes.ARRAY(DataTypes.ROW(
                                 DataTypes.FIELD("productid", DataTypes.INT()),
-                                DataTypes.FIELD("quantity", DataTypes.INT()),
-                                DataTypes.FIELD("unit_price", DataTypes.DECIMAL(9,3)),
-                                DataTypes.FIELD("discount", DataTypes.DECIMAL(9,3))
+                                DataTypes.FIELD("quantity", DataTypes.INT())
+//                                DataTypes.FIELD("unit_price", DataTypes.DECIMAL(9,3)),
+//                                DataTypes.FIELD("discount", DataTypes.DECIMAL(9,3))
                                 )))
                         .build())
                 .option("path", C360Test.RETAIL_DATA_DIR.toAbsolutePath() + "/orders.json")
@@ -73,28 +76,32 @@ public class FlinkTest {
 
 
         tEnv.createTable("Orders",jsonTable);
-
         Table orders = tEnv.from("Orders");
+//        Table flattenEntries = orders.select($("entries").at(1).flatten().get("quantity").flatten().sum().as("totalquant"));
 
-        Table count = orders.select($("entries").count().as("sum"));
-
-        Table flattenEntries = orders.select($("entries").at(1).get("productid").as("productid"));
+        Table shreddedEntries = tEnv.sqlQuery("SELECT  o.id, o.customerid, items.productid, items.quantity \n" +
+                "FROM Orders o CROSS JOIN UNNEST(o.entries) AS items (productid, quantity)");
 
 //        TableFunction func = new FlattenFunction();
 //        tEnv.createTemporarySystemFunction("flattenEntries", func);
-//        Table res = orders.flatMap(call("flattenEntries", $("entries").as("a", "b", "c", "d")));
+//        Table res = orders.flatMap(call("flattenEntries", $("entries")));
 
-        tEnv.toChangelogStream(flattenEntries).print();
+        tEnv.toChangelogStream(shreddedEntries).print();
 
         env.execute();
     }
 
+    @FunctionHint(
+            input = {@DataTypeHint("ARRAY<ROW<productid INT, quantity INT>>")},
+            output = @DataTypeHint("ROW<productid INT, quantity INT>")
+    )
   public class FlattenFunction extends TableFunction<Row> {
    public void eval(Row... args) {
      for (Row i : args) {
        collect(i);
      }
    }
+
  }
 
 
