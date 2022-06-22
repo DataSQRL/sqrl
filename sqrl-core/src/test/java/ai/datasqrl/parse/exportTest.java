3afe0a7d167042c9c5e78f0f4ca865f032b9cda8
Brad Bailey <bbailey7@uoregon.edu>
2022-06-21 18:13:00 -0700

    matched test to ticket

--- sqrl-core/src/test/java/ai/datasqrl/parse/exportTest.java
+++ sqrl-core/src/test/java/ai/datasqrl/parse/exportTest.java
@@ -10,7 +10,7 @@ class exportTest {
     public void exportTest() {
         SqrlParser parser = new SqrlParser(new SqrlParserOptions());
         // cannot use ANTLR defined keywords e.g. 'table' or 'source'
-        SqrlStatement sqrlStatement = parser.createStatement("EXPORT data1.json TO sink1.csv", ParsingOptions.builder().build());
+        SqrlStatement sqrlStatement = parser.createStatement("EXPORT UserAlerts TO file-output.Alerts", ParsingOptions.builder().build());
         System.out.println(NodeFormatter.accept(sqrlStatement));
     }
 }
