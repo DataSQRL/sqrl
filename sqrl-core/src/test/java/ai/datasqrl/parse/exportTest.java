ae97acd772bcfd8385f2d187ab16d5b12807e651
Brad Bailey <bbailey7@uoregon.edu>
2022-06-22 18:46:14 -0700

    exlcuded junit from artifacts: vertx-junit5 and junit-jupiter. added dependency for artifact: junit-jupiter-engine. Forced java compiler use in maven compiler plugin to see test errors: APIServerTest, SchemaTest, and ExportTest don't work with these updates to the POM.

--- sqrl-core/src/test/java/ai/datasqrl/parse/exportTest.java
+++ sqrl-core/src/test/java/ai/datasqrl/parse/exportTest.java
@@ -2,18 +2,17 @@ package ai.datasqrl.parse;
 
 import ai.datasqrl.parse.tree.NodeFormatter;
 import ai.datasqrl.parse.tree.SqrlStatement;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
 
-class exportTest {
+class ExportTest {
 
     @Test
-    public void ExportTest() {
+    public void exportTest() {
         SqrlParser parser = new SqrlParser(new SqrlParserOptions());
         // cannot use ANTLR defined keywords e.g. 'table' or 'source'
         SqrlStatement sqrlStatement = parser.createStatement("EXPORT UserAlerts TO file-output.Alerts", ParsingOptions.builder().build());
         //System.out.println(NodeFormatter.accept(sqrlStatement));
-        assertEquals("EXPORT UserAlerts TO file-output.Alerts",
-                      NodeFormatter.accept(sqrlStatement));
+        Assertions.assertEquals("EXPORT UserAlerts TO file-output.Alerts", NodeFormatter.accept(sqrlStatement));
     }
 }
