7ccbb8819c9d1d6f91da32e31b5cbf95470a66b8
Brad Bailey <bbailey7@uoregon.edu>
2022-07-19 14:11:16 -0700

    refactored conversions, first implementation of rounding functions + calcite tests for each

--- sqrl-core/src/main/java/ai/datasqrl/plan/calcite/SqrlOperatorTable.java
+++ sqrl-core/src/main/java/ai/datasqrl/plan/calcite/SqrlOperatorTable.java
@@ -1,14 +1,13 @@
 package ai.datasqrl.plan.calcite;
 
 import ai.datasqrl.function.builtin.time.NumToTimestampFunction;
-import ai.datasqrl.function.builtin.time.RoundMonth;
 import ai.datasqrl.function.builtin.example.SqlMyFunction;
+import ai.datasqrl.function.builtin.time.RoundToMonthFunction;
+import ai.datasqrl.function.builtin.time.RoundToSecondFunction;
 import ai.datasqrl.function.builtin.time.StringToTimestampFunction;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.fun.Now;
-import org.apache.calcite.sql.fun.SqlAbstractTimeFunction;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
-import org.apache.calcite.sql.type.SqlTypeName;
 
 public class SqrlOperatorTable extends SqlStdOperatorTable {
 
@@ -19,6 +18,8 @@ public class SqrlOperatorTable extends SqlStdOperatorTable {
   public static final SqlFunction NOW = new Now();
   public static final NumToTimestampFunction NUM_TO_TIMESTAMP = new NumToTimestampFunction();
   public static final StringToTimestampFunction STRING_TO_TIMESTAMP = new StringToTimestampFunction();
+  public static final RoundToSecondFunction ROUND_TO_SECOND = new RoundToSecondFunction();
+  public static final RoundToMonthFunction ROUND_TO_MONTH = new RoundToMonthFunction();
 
   public static synchronized SqrlOperatorTable instance() {
     if (instance == null) {
