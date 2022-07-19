66dc982a27114605a6d690f82008ea60bca6764a
Brad Bailey <bbailey7@uoregon.edu>
2022-07-19 11:47:11 -0700

    first implementation of TS conversion functions

--- sqrl-core/src/main/java/ai/datasqrl/plan/calcite/SqrlOperatorTable.java
+++ sqrl-core/src/main/java/ai/datasqrl/plan/calcite/SqrlOperatorTable.java
@@ -1,7 +1,9 @@
 package ai.datasqrl.plan.calcite;
 
+import ai.datasqrl.function.builtin.time.NumToTimestampFunction;
 import ai.datasqrl.function.builtin.time.RoundMonth;
 import ai.datasqrl.function.builtin.example.SqlMyFunction;
+import ai.datasqrl.function.builtin.time.StringToTimestampFunction;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.fun.Now;
 import org.apache.calcite.sql.fun.SqlAbstractTimeFunction;
@@ -14,8 +16,9 @@ public class SqrlOperatorTable extends SqlStdOperatorTable {
 
   //SQRL functions here:
   public static final SqlMyFunction MY_FUNCTION = new SqlMyFunction();
-  public static final RoundMonth ROUNDTOMONTH = new RoundMonth();
   public static final SqlFunction NOW = new Now();
+  public static final NumToTimestampFunction NUM_TO_TIMESTAMP = new NumToTimestampFunction();
+  public static final StringToTimestampFunction STRING_TO_TIMESTAMP = new StringToTimestampFunction();
 
   public static synchronized SqrlOperatorTable instance() {
     if (instance == null) {
