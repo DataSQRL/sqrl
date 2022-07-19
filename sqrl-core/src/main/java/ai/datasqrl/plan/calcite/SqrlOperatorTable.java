ea8a0f75bf17f6c4d1335563166eca27569a0ab6
Brad Bailey <bbailey7@uoregon.edu>
2022-07-19 14:14:05 -0700

    added rounding funcs to sqrl op table

--- sqrl-core/src/main/java/ai/datasqrl/plan/calcite/SqrlOperatorTable.java
+++ sqrl-core/src/main/java/ai/datasqrl/plan/calcite/SqrlOperatorTable.java
@@ -1,10 +1,7 @@
 package ai.datasqrl.plan.calcite;
 
-import ai.datasqrl.function.builtin.time.NumToTimestampFunction;
+import ai.datasqrl.function.builtin.time.*;
 import ai.datasqrl.function.builtin.example.SqlMyFunction;
-import ai.datasqrl.function.builtin.time.RoundToMonthFunction;
-import ai.datasqrl.function.builtin.time.RoundToSecondFunction;
-import ai.datasqrl.function.builtin.time.StringToTimestampFunction;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.fun.Now;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
@@ -19,7 +16,11 @@ public class SqrlOperatorTable extends SqlStdOperatorTable {
   public static final NumToTimestampFunction NUM_TO_TIMESTAMP = new NumToTimestampFunction();
   public static final StringToTimestampFunction STRING_TO_TIMESTAMP = new StringToTimestampFunction();
   public static final RoundToSecondFunction ROUND_TO_SECOND = new RoundToSecondFunction();
+  public static final RoundToMinuteFunction ROUND_TO_MINUTE = new RoundToMinuteFunction();
+  public static final RoundToHourFunction ROUND_TO_HOUR = new RoundToHourFunction();
+  public static final RoundToDayFunction ROUND_TO_DAY = new RoundToDayFunction();
   public static final RoundToMonthFunction ROUND_TO_MONTH = new RoundToMonthFunction();
+  public static final RoundToYearFunction ROUND_TO_YEAR = new RoundToYearFunction();
 
   public static synchronized SqrlOperatorTable instance() {
     if (instance == null) {
