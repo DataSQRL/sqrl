95efb7ecff8c697a70372772ea0f042dbad73698
Daniel Henneberger <git@danielhenneberger.com>
2022-11-12 10:45:42 -0800

    Update test case
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-core/src/test/java/ai/datasqrl/graphql/inference/SchemaInferenceModelTest.java
+++ sqrl-core/src/test/java/ai/datasqrl/graphql/inference/SchemaInferenceModelTest.java
@@ -1,22 +1,31 @@
 package ai.datasqrl.graphql.inference;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import ai.datasqrl.AbstractLogicalSQRLIT;
 import ai.datasqrl.IntegrationTestSettings;
 import ai.datasqrl.config.error.ErrorCollector;
 import ai.datasqrl.graphql.generate.SchemaGenerator;
 import ai.datasqrl.graphql.inference.SchemaInferenceModel.InferredSchema;
+import ai.datasqrl.graphql.server.Model.Root;
 import ai.datasqrl.parse.ConfiguredSqrlParser;
+import ai.datasqrl.physical.PhysicalPlan;
+import ai.datasqrl.plan.global.DAGPlanner;
+import ai.datasqrl.plan.global.OptimizedDAG;
 import ai.datasqrl.plan.local.generate.Resolve.Env;
+import ai.datasqrl.plan.queries.APIQuery;
 import ai.datasqrl.util.data.Retail;
 import ai.datasqrl.util.data.Retail.RetailScriptNames;
+import ai.datasqrl.util.db.JDBCTempDatabase;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.SchemaPrinter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import lombok.SneakyThrows;
+import org.apache.calcite.jdbc.CalciteSchema;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -39,12 +48,27 @@ class SchemaInferenceModelTest extends AbstractLogicalSQRLIT {
     String schemaStr = new String(Files.readAllBytes(schema));
     Env env = compile(Files.readString(example.getScript(RetailScriptNames.FULL).getScript()));
 
-    GraphQLSchema schema1 = SchemaGenerator.generate(env);
-
+    //Inference
     SchemaInference inference = new SchemaInference(schemaStr, env.getRelSchema(), env.getSession().getPlanner()
         .getRelBuilder(), env.getSession().getPlanner());
-    InferredSchema s = inference.accept();
+    InferredSchema inferredSchema = inference.accept();
 
-    assertEquals(64, s.getQuery().getFields().size());
+    assertEquals(64, inferredSchema.getQuery().getFields().size());
+
+    //Build queries
+    PgBuilder pgBuilder = new PgBuilder(schemaStr,
+        env.getRelSchema(),
+        env.getSession().getPlanner().getRelBuilder(),
+        env.getSession().getPlanner() );
+
+    Root root = inferredSchema.accept(pgBuilder, null);
+
+    List<APIQuery> queries = pgBuilder.getApiQueries();
+
+    assertEquals(444, queries.size());
+    /// plan dag
+    DAGPlanner dagPlanner = new DAGPlanner(env.getSession().getPlanner());
+    OptimizedDAG dag = dagPlanner.plan(env.getRelSchema(), queries, env.getExports(), env.getSession().getPipeline());
+
   }
 }
