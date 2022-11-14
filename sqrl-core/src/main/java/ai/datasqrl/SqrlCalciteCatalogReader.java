811939a5c6335e956f532c2d7d5ab1305c943a80
Daniel Henneberger <git@danielhenneberger.com>
2022-11-13 16:57:09 -0800

    Clean up calcite schema
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-core/src/main/java/ai/datasqrl/SqrlCalciteCatalogReader.java
+++ sqrl-core/src/main/java/ai/datasqrl/SqrlCalciteCatalogReader.java
@@ -3,14 +3,9 @@ package ai.datasqrl;
 import java.util.List;
 import lombok.Getter;
 import org.apache.calcite.config.CalciteConnectionConfig;
-import org.apache.calcite.jdbc.CalciteSchema;
 import org.apache.calcite.jdbc.SqrlCalciteSchema;
 import org.apache.calcite.prepare.CalciteCatalogReader;
-import org.apache.calcite.prepare.Prepare.PreparingTable;
-import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
-import org.apache.calcite.sql.SqlIdentifier;
-import org.apache.calcite.sql.validate.SqlValidatorUtil;
 
 public class SqrlCalciteCatalogReader extends CalciteCatalogReader {
 
@@ -23,23 +18,5 @@ public class SqrlCalciteCatalogReader extends CalciteCatalogReader {
       CalciteConnectionConfig config) {
     super(rootSchema, defaultSchema, typeFactory, config);
     this.sqrlRootSchema = rootSchema;
-  }
-
-  @Override
-  public PreparingTable getTableForMember(List<String> names) {
-    return this.getTable(names);
-
-//    SQRLTable table = super.getTableForMember(names).unwrap(SQRLTable.class);
-//    VirtualRelationalTable vt = sqrlRootSchema.getMapping().get(table);
-//    PreparingTable pt = this.getTable(List.of("", vt.getNameId()));
-//    return pt;
-  }
-
-  public RelDataType getNamedType(SqlIdentifier typeName) {
-    //We don't want to use the arbitrary nesting schema feature here
-    if (typeName.names.size() > 1) return null;
-
-    CalciteSchema.TypeEntry typeEntry = SqlValidatorUtil.getTypeEntry(this.getRootSchema(), typeName);
-    return typeEntry != null ? (RelDataType)typeEntry.getType().apply(this.typeFactory) : null;
   }
 }
