f5f2dd71abb89b2be2c53d583eee694af263170c
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2022-11-14 12:00:24 -0800

    Custom selectivity and rowcount provider

---
+++ sqrl-core/src/main/java/ai/datasqrl/plan/calcite/rules/SqrlRelMdRowCount.java
@@ -0,0 +1,20 @@
+package ai.datasqrl.plan.calcite.rules;
+
+import org.apache.calcite.adapter.enumerable.EnumerableNestedLoopJoin;
+import org.apache.calcite.rel.core.Join;
+import org.apache.calcite.rel.metadata.RelMdRowCount;
+import org.apache.calcite.rel.metadata.RelMetadataQuery;
+
+public class SqrlRelMdRowCount extends RelMdRowCount {
+
+    public Double getRowCount(Join rel, RelMetadataQuery mq) {
+        double rowCount = super.getRowCount(rel,mq);
+        if (rel instanceof EnumerableNestedLoopJoin) {
+            rowCount = rowCount + 2 * mq.getRowCount(rel.getLeft());
+            //Undo the factor 10 penalty from EnumerableNestedLoopJoin
+            rowCount = rowCount/10;
+        }
+        return rowCount;
+    }
+
+}
--- sqrl-core/src/main/java/ai/datasqrl/plan/calcite/rules/SqrlRelMdSelectivity.java
+++ sqrl-core/src/main/java/ai/datasqrl/plan/calcite/rules/SqrlRelMdSelectivity.java
@@ -1,9 +1,7 @@
 package ai.datasqrl.plan.calcite.rules;
 
-import org.apache.calcite.adapter.enumerable.EnumerableNestedLoopJoin;
 import org.apache.calcite.rel.core.Join;
 import org.apache.calcite.rel.metadata.RelMdSelectivity;
-import org.apache.calcite.rel.metadata.RelMdUtil;
 import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.calcite.rex.RexNode;
 
@@ -11,10 +9,6 @@ public class SqrlRelMdSelectivity extends RelMdSelectivity {
 
     @Override
     public Double getSelectivity(Join rel, RelMetadataQuery mq, RexNode predicate) {
-        if (rel instanceof EnumerableNestedLoopJoin) {
-            return RelMdUtil.guessSelectivity(predicate) / 100;
-        } else {
-            return super.getSelectivity(rel, mq, predicate);
-        }
+        return super.getSelectivity(rel, mq, predicate);
     }
 }
