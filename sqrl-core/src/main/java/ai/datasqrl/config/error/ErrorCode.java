ec6cb21a0329cf45b49bdd392f4b7a3a34b28826
Daniel Henneberger <git@danielhenneberger.com>
2022-11-17 12:47:55 -0800

    Convert to enum
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-core/src/main/java/ai/datasqrl/config/error/ErrorCode.java
+++ sqrl-core/src/main/java/ai/datasqrl/config/error/ErrorCode.java
@@ -8,22 +8,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import lombok.SneakyThrows;
 
-public class ErrorCode {
-  public static final ErrorCode GENERIC_ERROR = new ErrorCode("E0000.MD");
-  public static final ErrorCode IMPORT_NAMESPACE_CONFLICT = new ErrorCode("E0001.MD");
-  public static final ErrorCode IMPORT_CANNOT_BE_ALIASED = new ErrorCode("E0002.MD");
-  public static final ErrorCode IMPORT_STAR_CANNOT_HAVE_TIMESTAMP = new ErrorCode("E0003.MD");
-  public static final ErrorCode IMPORT_IN_HEADER = new ErrorCode("E0004.MD");
-  public static final ErrorCode MISSING_DEST_TABLE = new ErrorCode("E0005.MD");
-  public static final ErrorCode TIMESTAMP_COLUMN_MISSING = new ErrorCode("E0006.MD");
-  public static final ErrorCode TIMESTAMP_COLUMN_EXPRESSION = new ErrorCode("E0007.MD");
-  public static final ErrorCode PATH_CONTAINS_RELATIONSHIP = new ErrorCode("E0008.MD");
-  public static final ErrorCode MISSING_FIELD = new ErrorCode("E0009.MD");
-  public static final ErrorCode MISSING_TABLE = new ErrorCode("E0010.MD");
-  public static final ErrorCode ORDINAL_NOT_SUPPORTED = new ErrorCode("E0011.MD");
-  public static final ErrorCode CANNOT_SHADOW_RELATIONSHIP = new ErrorCode("E0012.MD");
-  public static final ErrorCode TO_MANY_PATH_NOT_ALLOWED = new ErrorCode("E0013.MD");
-  public static final ErrorCode NESTED_DISTINCT_ON = new ErrorCode("E0014.MD");
+public enum ErrorCode {
+  GENERIC_ERROR("E0000.MD"),
+  IMPORT_NAMESPACE_CONFLICT("E0001.MD"),
+  IMPORT_CANNOT_BE_ALIASED("E0002.MD"),
+  IMPORT_STAR_CANNOT_HAVE_TIMESTAMP("E0003.MD"),
+  IMPORT_IN_HEADER("E0004.MD"),
+  MISSING_DEST_TABLE("E0005.MD"),
+  TIMESTAMP_COLUMN_MISSING("E0006.MD"),
+  TIMESTAMP_COLUMN_EXPRESSION("E0007.MD"),
+  PATH_CONTAINS_RELATIONSHIP("E0008.MD"),
+  MISSING_FIELD("E0009.MD"),
+  MISSING_TABLE("E0010.MD"),
+  ORDINAL_NOT_SUPPORTED("E0011.MD"),
+  CANNOT_SHADOW_RELATIONSHIP("E0012.MD"),
+  TO_MANY_PATH_NOT_ALLOWED("E0013.MD"),
+  NESTED_DISTINCT_ON("E0014.MD")
+  ;
 
   final URI file;
 
