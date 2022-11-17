e9eb9720dd652ba264562915bf4d3065672ffd7f
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2022-11-17 13:15:54 -0800

    Updated query use case test.

--- sqrl-core/src/test/java/ai/datasqrl/flink/FlinkQueryUseCaseTest.java
+++ sqrl-core/src/test/java/ai/datasqrl/flink/FlinkQueryUseCaseTest.java
@@ -5,14 +5,12 @@ import ai.datasqrl.IntegrationTestSettings;
 import ai.datasqrl.util.SnapshotTest;
 import ai.datasqrl.util.TestGraphQLSchema;
 import ai.datasqrl.util.TestScript;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 public class FlinkQueryUseCaseTest extends AbstractPhysicalSQRLIT {
 
     @ParameterizedTest
-    @Disabled
     @ArgumentsSource(TestScript.AllScriptsWithGraphQLSchemaProvider.class)
     public void fullScriptTest(TestScript script, TestGraphQLSchema graphQLSchema) {
         snapshot = SnapshotTest.Snapshot.of(getClass(), script.getName(), graphQLSchema.getName());
