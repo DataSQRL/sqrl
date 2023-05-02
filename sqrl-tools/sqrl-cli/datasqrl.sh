47ffd21694abbc77822f522e4e558695276b7de0
henneberger <git@danielhenneberger.com>
2023-05-02 13:10:49 -0700

    Adjust code for release (#244)
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-tools/sqrl-cli/datasqrl.sh
+++ sqrl-tools/sqrl-cli/datasqrl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-java -jar ${parent_path}/target/sqrl-cli-0.1-SNAPSHOT-shaded.jar ${@}
+java -jar ${parent_path}/target/sqrl-cli-0.1.0-SNAPSHOT-shaded.jar ${@}
