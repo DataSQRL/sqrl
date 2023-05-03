179be706f1ee6164dd9e283c9c7f3b4c35fdccc2
Daniel Henneberger <git@danielhenneberger.com>
2023-05-03 10:01:34 -0700

    Remove unused engines, add logging
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-engines/sqrl-engine-server/src/main/java/com/datasqrl/engine/server/LambdaNativeEngineFactory.java
+++ sqrl-engines/sqrl-engine-server/src/main/java/com/datasqrl/engine/server/LambdaNativeEngineFactory.java
@@ -7,14 +7,16 @@ import com.google.auto.service.AutoService;
 import java.nio.file.Path;
 import java.time.Duration;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
 import org.testcontainers.utility.MountableFile;
 
+@Slf4j
 @AutoService(EngineFactory.class)
 public class LambdaNativeEngineFactory extends GraphqlServerEngineFactory {
 
-  public static final String ENGINE_NAME = "aws-lambda-native-arm64";
+  public static final String ENGINE_NAME = "aws-lambda-native";
 
   @Override
   public String getEngineName() {
@@ -34,16 +36,17 @@ public class LambdaNativeEngineFactory extends GraphqlServerEngineFactory {
 
     @Override
     public void generateAssets(Path buildDir) {
+      log.info("Generating lambda assets.");
 
       Path sourcePath = buildDir;
       String targetPath = "/build";
       MountableFile mountableFile = MountableFile.forHostPath(sourcePath);
 
-      GenericContainer<?> container = new GenericContainer<>("engine-aws-lambda-native")
+      GenericContainer<?> container = new GenericContainer<>("datasqrl/engine-aws-lambda-native")
           .withStartupCheckStrategy(new OneShotStartupCheckStrategy()
               .withTimeout(Duration.ofMinutes(10)))
-          .withFileSystemBind(mountableFile.getResolvedPath(), targetPath);
-
+          .withFileSystemBind(mountableFile.getResolvedPath(), targetPath)
+          .withLogConsumer((frame)->log.info(frame.getUtf8String()));
       container.start();
       container.stop();
     }
--- sqrl-execute/sqrl-execute-http/sqrl-execute-http-aws-lambda/entrypoint-native-image.sh
+++ sqrl-execute/sqrl-execute-http/sqrl-execute-http-aws-lambda/entrypoint-native-image.sh
@@ -1,6 +1,5 @@
 #!bin/sh
 
-cd /build
 # TODO: fix validation by rerunning graalvm agent
 native-image -jar /sqrl-execute-http-aws-lambda.jar -H:ConfigurationFileDirectories=/config-dir --static -H:IncludeResources=i18n/Validation.*
 
