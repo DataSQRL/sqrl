b1490805abd114e4e2b446c81afb9118761bb732
Daniel Henneberger <git@danielhenneberger.com>
2023-05-23 13:18:14 -0700

    More case insensitive paths
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-base/src/main/java/com/datasqrl/util/NameUtil.java
+++ sqrl-base/src/main/java/com/datasqrl/util/NameUtil.java
@@ -2,7 +2,10 @@ package com.datasqrl.util;
 
 import com.datasqrl.canonicalizer.Name;
 import com.datasqrl.canonicalizer.NamePath;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
+import lombok.SneakyThrows;
 
 public class NameUtil {
 
@@ -10,8 +13,22 @@ public class NameUtil {
     Path filePath = basePath;
     for (int i = 0; i < path.getNames().length; i++) {
       Name name = path.getNames()[i];
-      filePath = filePath.resolve(name.getDisplay());
+      filePath = getCaseInsensitivePath(filePath, name.getDisplay());
     }
     return filePath;
+  }
+
+  @SneakyThrows
+  public static Path getCaseInsensitivePath(Path filePath, String name) {
+    //check for exact match
+    if (Files.exists(filePath.resolve(name))) {
+      return filePath.resolve(name);
+    }
+
+    //find first inexact match, or else just the name provided
+    return Files.list(filePath)
+        .filter(path -> path.getFileName().toString().toLowerCase().equals(name))
+        .findFirst()
+        .orElse(filePath.resolve(name));
   }
 }
