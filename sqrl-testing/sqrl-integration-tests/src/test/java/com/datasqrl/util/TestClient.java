724a78eb67ff6a4eb064b1aba759b34bc37a6059
Daniel Henneberger <git@danielhenneberger.com>
2023-09-21 16:30:34 -0700

    Remove log line
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/util/TestClient.java
+++ sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/util/TestClient.java
@@ -18,7 +18,6 @@ public class TestClient {
 
   public static final Consumer<HttpResponse<JsonObject>> NO_HANDLER = (h)->{};
   public static final Consumer<HttpResponse<JsonObject>> FAIL_HANDLER = (h)->{
-    System.out.println("Got response"+h.body().encode());
     if (h.statusCode() >= 300) {
       fail(h.statusMessage());
     }};
