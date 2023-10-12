9cebe097144647338dee5be7fb4b5e6e8e627e7e
henneberger <git@danielhenneberger.com>
2023-10-12 14:28:57 -0700

    Update library to use bridging flink functions (#368)
    * Use ungeneralized types for flexible schema and planner
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    * Update other file
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    * Update library to use briding flink functions
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    ---------
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-engines/sqrl-engine-core/src/main/java/com/datasqrl/NamespaceObjectUtil.java
+++ sqrl-engines/sqrl-engine-core/src/main/java/com/datasqrl/NamespaceObjectUtil.java
@@ -1,34 +1,50 @@
 package com.datasqrl;
 
+import static com.datasqrl.util.FunctionUtil.getFunctionByNameFromClass;
+
+import com.datasqrl.calcite.type.TypeFactory;
 import com.datasqrl.canonicalizer.Name;
+import com.datasqrl.flink.FlinkConverter;
 import com.datasqrl.function.CalciteFunctionNsObject;
 import com.datasqrl.function.FlinkUdfNsObject;
 import com.datasqrl.function.SqrlFunction;
 import com.datasqrl.util.FunctionUtil;
 import com.datasqrl.module.NamespaceObject;
 import com.google.common.base.Preconditions;
+import java.util.Locale;
 import java.util.Optional;
+import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.sql.SqlFunction;
+import org.apache.calcite.sql.SqlOperator;
+import org.apache.calcite.sql.fun.SqlStdOperatorTable;
+import org.apache.flink.table.functions.BuiltInFunctionDefinition;
+import org.apache.flink.table.functions.BuiltInFunctionDefinitions;
 import org.apache.flink.table.functions.FunctionDefinition;
-import org.apache.flink.table.functions.ScalarFunction;
-import org.apache.flink.table.planner.functions.sql.FlinkSqlOperatorTable;
 
 public class NamespaceObjectUtil {
-
-  public static NamespaceObject createNsObject(String name, SqlFunction fnc) {
-    return new CalciteFunctionNsObject(Name.system(name), fnc, "");
-  }
 
   public static NamespaceObject createFunctionFromFlink(String name) {
     return createFunctionFromFlink(name, name);
   }
 
   public static NamespaceObject createFunctionFromFlink(String name, String originalName) {
-    Optional<SqlFunction> function = FunctionUtil.getFunctionByNameFromClass(FlinkSqlOperatorTable.class, originalName);
-    Preconditions.checkArgument(function.isPresent());
-    return new CalciteFunctionNsObject(Name.system(name), function.get(), originalName);
+    FlinkConverter converter = new FlinkConverter(TypeFactory.getTypeFactory());
+
+    Optional<BuiltInFunctionDefinition> function = getFunctionByNameFromClass(BuiltInFunctionDefinitions.class,
+        BuiltInFunctionDefinition.class,
+        originalName.toUpperCase(Locale.ROOT));
+    Preconditions.checkArgument(function.isPresent(), "Could not find function %s", name);
+    BuiltInFunctionDefinition fnc = function.get();
+    SqlFunction function1 = converter.convertFunction(originalName, fnc);
+
+    return new CalciteFunctionNsObject(Name.system(name), function1, originalName);
   }
 
+  public static NamespaceObject createFunctionFromStdOpTable(String name) {
+    return new CalciteFunctionNsObject(Name.system(name),
+        getFunctionByNameFromClass(SqlStdOperatorTable.class,
+            SqlOperator.class, name).get(), name);
+  }
 
   public static NamespaceObject createNsObject(SqrlFunction function) {
     Preconditions.checkArgument(function instanceof FunctionDefinition,
