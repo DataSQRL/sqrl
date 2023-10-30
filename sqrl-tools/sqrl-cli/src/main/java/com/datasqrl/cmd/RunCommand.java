a3846c53e414640f8ee94915bcae1c8b6e9beb92
Daniel Henneberger <git@danielhenneberger.com>
2023-10-30 00:10:32 -0700

    Reinstate run command
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-tools/sqrl-cli/src/main/java/com/datasqrl/cmd/RunCommand.java
+++ sqrl-tools/sqrl-cli/src/main/java/com/datasqrl/cmd/RunCommand.java
@@ -3,6 +3,15 @@
  */
 package com.datasqrl.cmd;
 
+import com.datasqrl.compile.Compiler;
+import com.datasqrl.config.SqrlConfig;
+import com.datasqrl.error.ErrorCollector;
+import com.datasqrl.packager.Packager;
+import com.datasqrl.service.PackagerUtil;
+import com.google.common.base.Preconditions;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "run", description = "Compiles a SQRL script and runs the entire generated data pipeline")
@@ -12,4 +21,39 @@ public class RunCommand extends AbstractCompilerCommand {
     super(true, true, true);
   }
 
+
+  public void runCommand(ErrorCollector errors) {
+    if (DEFAULT_DEPLOY_DIR.equals(targetDir)) {
+      targetDir = root.rootDir.resolve(targetDir);
+    }
+
+    DefaultConfigSupplier configSupplier = new DefaultConfigSupplier(errors);
+
+    SqrlConfig config = getDefaultConfig(true, errors)
+        .get();
+
+    Packager packager = PackagerUtil.create(root.rootDir, files, config, errors);
+    packager.cleanUp();
+    Path packageFilePath =  packager.populateBuildDir(!noinfer);
+    if (errors.hasErrors()) {
+      return;
+    }
+
+    Compiler compiler = new Compiler();
+    Preconditions.checkArgument(Files.isRegularFile(packageFilePath));
+    Compiler.CompilerResult result = compiler.run(errors, packageFilePath.getParent(), debug, targetDir);
+
+    if (errors.hasErrors()) {
+      return;
+    }
+    if (configSupplier.usesDefault) {
+      addDockerCompose(Optional.ofNullable(mountDirectory));
+      addFlinkExecute();
+    }
+    if (isGenerateGraphql()) {
+      addGraphql(packager.getBuildDir(), packager.getRootDir());
+    }
+
+    executePlan(result.getPlan(), errors);
+  }
 }
