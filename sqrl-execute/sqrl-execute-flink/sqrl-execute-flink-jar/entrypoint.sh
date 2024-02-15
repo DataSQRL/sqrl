297342fadb349ec8d0aebcd8969b5bb3a4c2ccd8
Daniel Henneberger <git@danielhenneberger.com>
2024-02-15 13:03:50 -0800

    Clean up docker build
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-execute/sqrl-execute-flink/sqrl-execute-flink-jar/entrypoint.sh
+++ sqrl-execute/sqrl-execute-flink/sqrl-execute-flink-jar/entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 export SQRL_JAR_PATH=/sqrl.jar
 cd /build
-#cp /build.gradle build.gradle
+# avoid including sql by default
+if [ -f /build/deploy/flink-plan.sql ]; then
+    mv /build/deploy/flink-plan.sql /build/flink-plan.sql
+fi
+
+if [ -f /build/deploy/flink-job.jar ]; then
+    rm /build/deploy/flink-job.jar
+fi
+
 gradle clean shadowJar
 mkdir -p /build/deploy/
 mv /build/build/libs/build-all.jar /build/deploy/flink-job.jar
+
+if [ -f /build/deploy/flink-plan.sql ]; then
+    mv /build/flink-plan.sql /build/deploy/flink-plan.sql
+fi
 gradle clean
 echo "Done."
