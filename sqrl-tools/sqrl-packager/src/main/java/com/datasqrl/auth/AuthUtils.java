85b5d13ec1ded3377da1255615f574a6774f1f90
Nandor Soma Abonyi <nandor@datasqrl.com>
2024-05-11 23:58:47 +0200

    latest audience

--- sqrl-tools/sqrl-packager/src/main/java/com/datasqrl/auth/AuthUtils.java
+++ sqrl-tools/sqrl-packager/src/main/java/com/datasqrl/auth/AuthUtils.java
@@ -7,7 +7,7 @@ public class AuthUtils {
   public static final String REDIRECT_URI = String.format("http://localhost:%d%s", CALLBACK_SERVER_PORT, CALLBACK_ENDPOINT);
 
   public static final String CLIENT_ID = "KC2EoCphVVe5wzZj3GyFiTZboulfYJNH";
-  public static final String AUDIENCE = "https://dev-46466kz3hleg0in1.us.auth0.com/api/v2/";
+  public static final String AUDIENCE = "https://sqrl-repository-frontend-git-staging-datasqrl.vercel.app";
   public static final String AUTH0_BASE_URL = "https://sqrl-repository-frontend-git-staging-datasqrl.vercel.app/auth0";
   public static final String TOKEN_ENDPOINT = AUTH0_BASE_URL + "/oauth/token";
   public static final String AUTHORIZE_ENDPOINT = AUTH0_BASE_URL + "/authorize";
