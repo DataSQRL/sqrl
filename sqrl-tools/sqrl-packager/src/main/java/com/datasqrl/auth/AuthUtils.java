aa09797ca7c82f45ad4f9171a6d376d21de295c2
Daniel Henneberger <git@danielhenneberger.com>
2024-05-21 12:05:32 -0700

    Move to production repo
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-tools/sqrl-packager/src/main/java/com/datasqrl/auth/AuthUtils.java
+++ sqrl-tools/sqrl-packager/src/main/java/com/datasqrl/auth/AuthUtils.java
@@ -6,9 +6,9 @@ public class AuthUtils {
   public static final String CALLBACK_ENDPOINT = "/api/auth/callback";
   public static final String REDIRECT_URI = String.format("http://localhost:%d%s", CALLBACK_SERVER_PORT, CALLBACK_ENDPOINT);
 
-  public static final String CLIENT_ID = "KC2EoCphVVe5wzZj3GyFiTZboulfYJNH";
-  public static final String AUDIENCE = "https://sqrl-repository-frontend-git-staging-datasqrl.vercel.app";
-  public static final String AUTH0_BASE_URL = "https://sqrl-repository-frontend-git-staging-datasqrl.vercel.app/auth0";
+  public static final String CLIENT_ID = "Ov9VIEZyzsxRsnKRcgbSwOsXfc6WUBUU";
+  public static final String AUDIENCE = "https://dev.datasqrl.com";
+  public static final String AUTH0_BASE_URL = "https://dev.datasqrl.com/auth0";
   public static final String TOKEN_ENDPOINT = AUTH0_BASE_URL + "/oauth/token";
   public static final String AUTHORIZE_ENDPOINT = AUTH0_BASE_URL + "/authorize";
 }
