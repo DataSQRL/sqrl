c5b7ee067292dea1c4eaa630c751ea9ff209f4a7
henneberger <git@danielhenneberger.com>
2024-07-08 16:42:35 -0700

    Fix cardinality constraint (#711)
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-flink-lib/sqrl-flexible-json/src/main/java/com/datasqrl/schema/input/external/FieldDefinition.java
+++ sqrl-flink-lib/sqrl-flexible-json/src/main/java/com/datasqrl/schema/input/external/FieldDefinition.java
@@ -6,29 +6,18 @@ package com.datasqrl.schema.input.external;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Getter
 public class FieldDefinition extends AbstractElementDefinition implements FieldTypeDefinition {
 
   public String type;
   public List<FieldDefinition> columns;
   public List<String> tests;
+  public Map<String, Object> cardinality;
 
   public Map<String, FieldTypeDefinitionImpl> mixed;
 
-  @Override
-  public String getType() {
-    return type;
-  }
-
-  @Override
-  public List<FieldDefinition> getColumns() {
-    return columns;
-  }
-
-  @Override
-  public List<String> getTests() {
-    return tests;
-  }
 }
