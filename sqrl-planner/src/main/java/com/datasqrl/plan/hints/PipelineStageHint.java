5b1326c7df5dd94b99816edd11bb39d4f3300ca3
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2024-07-11 11:35:01 -0700

    Validating that our hinting framework can accommodate optimizer hints for the dag planner. Some minor refactoring to make it cleaner.
    Signed-off-by: Matthias Broecheler <mbroecheler@users.noreply.github.com>

---
+++ sqrl-planner/src/main/java/com/datasqrl/plan/hints/PipelineStageHint.java
@@ -0,0 +1,25 @@
+package com.datasqrl.plan.hints;
+
+import com.datasqrl.engine.pipeline.ExecutionPipeline;
+import com.datasqrl.engine.pipeline.ExecutionStage;
+import com.datasqrl.error.ErrorCollector;
+import java.util.Optional;
+import lombok.Value;
+
+@Value
+public
+class PipelineStageHint implements OptimizerHint {
+
+  public static final String HINT_NAME = "exec";
+
+  String stageName;
+
+  public ExecutionStage getStage(ExecutionPipeline pipeline, ErrorCollector errors) {
+    Optional<ExecutionStage> stage = pipeline.getStage(stageName)
+        .or(() -> pipeline.getStageByType(stageName));
+    errors.checkFatal(stage.isPresent(),
+        "Could not find execution stage [%s] specified in optimizer hint", stageName);
+    return stage.get();
+  }
+
+}
--- sqrl-planner/src/main/java/com/datasqrl/plan/rules/LPAnalysis.java
+++ sqrl-planner/src/main/java/com/datasqrl/plan/rules/LPAnalysis.java
@@ -1,10 +1,12 @@
 package com.datasqrl.plan.rules;
 
 import com.datasqrl.plan.hints.OptimizerHint;
+import com.datasqrl.plan.hints.PipelineStageHint;
 import java.util.List;
 import lombok.NonNull;
 import lombok.Value;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.sql.SqlHint;
 
 @Value
 public class LPAnalysis {
@@ -13,9 +15,12 @@ public class LPAnalysis {
 
   @NonNull AnnotatedLP convertedRelnode;
 
-  @NonNull List<OptimizerHint.Stage> configuredStages;
+  @NonNull List<PipelineStageHint> configuredStages;
 
   @NonNull SqrlConverterConfig converterConfig;
 
+  @NonNull List<OptimizerHint> sqrlHints;
+
+  @NonNull List<SqlHint> sqlHints;
 
 }
