0ead2d871487bc16530be6d9bd51729573e81a5e
henneberger <git@danielhenneberger.com>
2024-07-16 11:23:50 -0700

    Remove specific table imports in favor of future schema isolation (#717)
    * Do not import specific tables
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    * Update tests
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    ---------
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-planner/src/main/java/com/datasqrl/loaders/ScriptSqrlModule.java
+++ sqrl-planner/src/main/java/com/datasqrl/loaders/ScriptSqrlModule.java
@@ -1,12 +1,11 @@
 package com.datasqrl.loaders;
 
-import static com.datasqrl.canonicalizer.Name.HIDDEN_PREFIX;
-
 import com.datasqrl.calcite.SqrlFramework;
 import com.datasqrl.calcite.function.SqrlTableMacro;
 import com.datasqrl.canonicalizer.Name;
 import com.datasqrl.config.PackageJson;
 import com.datasqrl.engine.log.LogManager;
+import com.datasqrl.canonicalizer.NamePath;
 import com.datasqrl.error.ErrorCollector;
 import com.datasqrl.module.NamespaceObject;
 import com.datasqrl.module.SqrlModule;
@@ -14,14 +13,10 @@ import com.datasqrl.plan.MainScript;
 import com.datasqrl.plan.validate.ScriptPlanner;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.AllArgsConstructor;
-import org.apache.calcite.jdbc.CalciteSchema.FunctionEntry;
-import org.apache.calcite.jdbc.CalciteSchema.FunctionEntryImpl;
 import org.apache.calcite.jdbc.SqrlSchema;
-import org.apache.calcite.schema.Function;
 
 @AllArgsConstructor
 public class ScriptSqrlModule implements SqrlModule {
@@ -31,6 +26,7 @@ public class ScriptSqrlModule implements SqrlModule {
   private Optional<SqrlSchema> schema;
   private PackageJson sqrlConfig;
   private LogManager logManager;
+  private final NamePath namePath;
 
   private final AtomicBoolean planned = new AtomicBoolean(false);
 
@@ -92,62 +88,29 @@ public class ScriptSqrlModule implements SqrlModule {
     @Override
     public boolean apply(ScriptPlanner planner, Optional<String> objectName,
         SqrlFramework framework, ErrorCollector errors) {
-      if (!planned.get()) {
-        planned.set(true);
-        plan(planner, objectName, framework, errors);
-      }
-
         //Add all non-sqrl tables to current schema (e.g. no table mappings)
-      SqrlSchema curSchema = framework.getSchema();
-      SqrlSchema scriptSchema = schema.get();
       if (tableName.isEmpty()) {
-        curSchema.getPathToAbsolutePathMap().putAll(scriptSchema.getPathToAbsolutePathMap());
-        curSchema.getPathToSysTableMap().putAll(scriptSchema.getPathToSysTableMap());
+        //Plan over current schema
+        if (!planned.get()) {
+          planned.set(true);
+          planner.plan(
+              new MainScript() {
+                @Override
+                public Optional<Path> getPath() {
+                  return Optional.empty();
+                }
 
-        curSchema.getUdf().putAll(scriptSchema.getUdf());
-        curSchema.getUdfListMap().putAll(scriptSchema.getUdfListMap());
-        curSchema.getTableNameToIdMap().putAll(scriptSchema.getTableNameToIdMap());
-        curSchema.getAddlSql().addAll(scriptSchema.getAddlSql());
-
-        for (Map.Entry<String, List<FunctionEntry>> entry : scriptSchema.getFunctionMap().map().entrySet()) {
-          if (!entry.getKey().startsWith(HIDDEN_PREFIX)) {
-            for (FunctionEntry entry1 : entry.getValue()) {
-              curSchema.getFunctionMap().put(entry.getKey(), entry1);
-            }
-          }
+                @Override
+                public String getContent() {
+                  return script;
+                }
+              }, moduleLoader);
         }
-
       } else {
-        Name table = tableName.get();
-        Name destName = objectName.map(o -> Name.system(o)).orElse(tableName.get());
-        curSchema.getPathToAbsolutePathMap().put(
-            destName.toNamePath(), scriptSchema.getPathToAbsolutePathMap().get(table.toNamePath()));
-        curSchema.getPathToSysTableMap().put(
-            destName.toNamePath(),
-            scriptSchema.getPathToSysTableMap().get(table.toNamePath()));
-
-        curSchema.getUdf().putAll(scriptSchema.getUdf());
-        curSchema.getUdfListMap().putAll(scriptSchema.getUdfListMap());
-        curSchema.getTableNameToIdMap().putAll(scriptSchema.getTableNameToIdMap());
-        curSchema.getAddlSql().addAll(scriptSchema.getAddlSql());
-
-        for (Map.Entry<String, List<FunctionEntry>> entry : scriptSchema.getFunctionMap().map().entrySet()) {
-          if (entry.getKey().equalsIgnoreCase(table.getCanonical())) {
-            for (FunctionEntry entry1 : entry.getValue()) {
-              Function function = entry1.getFunction();
-              if (function instanceof SqrlTableMacro) {
-                SqrlTableMacro sqrlTableMacro = (SqrlTableMacro) function;
-                SqrlTableMacro renamed = sqrlTableMacro.rename(destName);
-
-                curSchema.getFunctionMap().put(destName.getDisplay(),
-                    new FunctionEntryImpl(curSchema, destName.getDisplay(), renamed));
-              }
-
-            }
-          }
-        }
+        throw new RuntimeException(
+            String.format("Importing specific tables are not currently supported, please import all: IMPORT %s.*;",
+                namePath.popLast().toString()));
       }
-
 
       return true;
     }
