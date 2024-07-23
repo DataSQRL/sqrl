f54b77089083b62777aba4575014b7dccde16fe5
Nandor Soma Abonyi <nandor@datasqrl.com>
2024-07-23 17:47:46 +0200

    ugly hack to fix kafka, but we need a solution here, since for kafka, this shouldn't be a field

--- sqrl-calcite/src/main/java/com/datasqrl/plan/validate/ScriptPlanner.java
+++ sqrl-calcite/src/main/java/com/datasqrl/plan/validate/ScriptPlanner.java
@@ -158,7 +158,10 @@ public class ScriptPlanner implements StatementVisitor<Void, Void> {
     TypeFactory typeFactory = TypeFactory.getTypeFactory();
     RelDataTypeBuilder fieldBuilder = CalciteUtil.getRelTypeBuilder(typeFactory);
     fieldBuilder.add(Name.system("_uuid"), typeFactory.createSqlType(SqlTypeName.VARCHAR));
-    fieldBuilder.add(Name.system("event_time"), typeFactory.createSqlType(SqlTypeName.TIMESTAMP_WITH_LOCAL_TIME_ZONE, 3));
+    // TODO: that's an ugly hack, postgres-log requires it as a field, however kafka requires it as metadata
+    if (logManager.getEngineName().equals("postgres_log")) {
+      fieldBuilder.add(Name.system("event_time"), typeFactory.createSqlType(SqlTypeName.TIMESTAMP_WITH_LOCAL_TIME_ZONE, 3));
+    }
     for (SqrlColumnDefinition definition : statement.getColumns()) {
       fieldBuilder.add(definition.getColumnName().getSimple(),
           SqlDataTypeSpecBuilder.create(definition.getType(), typeFactory));
@@ -168,13 +171,6 @@ public class ScriptPlanner implements StatementVisitor<Void, Void> {
 
     Log sinkLog = logManager.create(logId, namePath.getLast(),
         relDataType, List.of("_uuid"), new Timestamp("event_time", TimestampType.LOG_TIME));
-
-    // TODO: do we really need this?
-    if (sinkLog == null) {
-      addError(ErrorCode.CANNOT_RESOLVE_TABLESINK, statement, "Cannot resolve log: %s",
-          logId);
-      return null;
-    }
 
     TableConfig tableConfig = sinkLog.getSource().getConfiguration();
     this.framework.getSchema().add(new ResolvedImport(logId, tableConfig));
