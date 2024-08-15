dbc84ec6bca0253d562059e13c9d8558be757ac5
Daniel Henneberger <git@danielhenneberger.com>
2024-08-15 16:25:02 -0700

    Add flink compiled plan step to test case
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-testing/sqrl-flink-1.18/src/test/java/com/datasqrl/IntTestPlanTest.java
+++ sqrl-testing/sqrl-flink-1.18/src/test/java/com/datasqrl/IntTestPlanTest.java
@@ -6,14 +6,13 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 import org.apache.flink.table.api.CompiledPlan;
-import org.apache.flink.table.api.TableResult;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // This is to allow the method source to not be static
-public class DagPlanPlannerTest {
+public class IntTestPlanTest {
 
   // Method to provide the directories as test arguments
   static Stream<Path> directoryProvider() {
@@ -37,11 +36,18 @@ public class DagPlanPlannerTest {
     return Stream.empty();
   }
 
+  static DatasqrlRun datasqrlRun;
+
+  @BeforeAll
+  static void beforeAll() {
+    datasqrlRun = new DatasqrlRun();
+    datasqrlRun.startKafkaCluster();
+  }
+
   @ParameterizedTest
   @MethodSource("directoryProvider")
   void testCompilePlanOnDirectory(Path directoryPath) {
-    DatasqrlRun datasqrlRun = new DatasqrlRun(directoryPath);
-
+    datasqrlRun.setPath(directoryPath);
     try {
       CompiledPlan plan = datasqrlRun.compileFlink();
       // plan.execute().print(); Uncomment if execution is required
