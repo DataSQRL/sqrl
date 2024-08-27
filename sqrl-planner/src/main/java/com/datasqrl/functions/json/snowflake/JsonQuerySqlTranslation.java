02b43d65aae81691eae9ed03b19b0bc8b04b48d2
Daniel Henneberger <git@danielhenneberger.com>
2024-08-27 12:26:41 -0700

    Disable json functions for snowflake due to issues
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-planner/src/main/java/com/datasqrl/functions/json/snowflake/JsonQuerySqlTranslation.java
+++ sqrl-planner/src/main/java/com/datasqrl/functions/json/snowflake/JsonQuerySqlTranslation.java
@@ -10,8 +10,8 @@ import com.google.auto.service.AutoService;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlWriter;
 import org.apache.calcite.sql.parser.SqlParserPos;
-
-@AutoService(SqlTranslation.class)
+//Disabled for now
+//@AutoService(SqlTranslation.class)
 public class JsonQuerySqlTranslation extends SnowflakeSqlTranslation {
 
   public JsonQuerySqlTranslation() {
--- sqrl-planner/src/main/java/com/datasqrl/functions/json/snowflake/ParseJsonFunction.java
+++
@@ -1,5 +0,0 @@
-package com.datasqrl.functions.json.snowflake;
-
-public class ParseJsonFunction {
-
-}
