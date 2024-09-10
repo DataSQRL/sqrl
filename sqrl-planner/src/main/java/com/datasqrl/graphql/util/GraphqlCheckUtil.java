6ebaa8b0c89e216594d0baeb5af7e65c00f34ee4
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2024-09-10 09:49:26 -0700

    Fixes #802 (#803)
    Fixes 2 things:
    1) Bug in error message for missing type. Fixes #802.
    2) The way we compute the profile path does not work for external use case tests.
    
    Signed-off-by: Matthias Broecheler <mbroecheler@users.noreply.github.com>

--- sqrl-planner/src/main/java/com/datasqrl/graphql/util/GraphqlCheckUtil.java
+++ sqrl-planner/src/main/java/com/datasqrl/graphql/util/GraphqlCheckUtil.java
@@ -7,17 +7,17 @@ import org.apache.calcite.sql.parser.SqlParserPos;
 
 public class GraphqlCheckUtil {
 
-  public static void checkState(boolean check, SourceLocation sourceLocation, String message, String... args) {
+  public static void checkState(boolean check, SourceLocation sourceLocation, String message, Object... args) {
     if (!check) {
       throw createThrowable(sourceLocation, message, args);
     }
   }
 
-  public static SqrlAstException createUnknownThrowable(String message, String... args) {
+  public static SqrlAstException createUnknownThrowable(String message, Object... args) {
     return createThrowable(new SourceLocation(0,0), message, args);
   }
 
-  public static SqrlAstException createThrowable(SourceLocation sourceLocation, String message, String... args) {
+  public static SqrlAstException createThrowable(SourceLocation sourceLocation, String message, Object... args) {
     return new SqrlAstException(ErrorLabel.GENERIC,
         toParserPos(sourceLocation),
         message, args);
