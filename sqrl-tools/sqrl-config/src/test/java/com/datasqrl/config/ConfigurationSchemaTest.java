5aea1bd23cd05e0b68708bc9ba40fa245f303963
Nandor Soma Abonyi <nandor@datasqrl.com>
2024-09-23 17:33:27 +0200

    restructure

--- sqrl-tools/sqrl-config/src/test/java/com/datasqrl/config/ConfigurationSchemaTest.java
+++ sqrl-tools/sqrl-config/src/test/java/com/datasqrl/config/ConfigurationSchemaTest.java
@@ -5,28 +5,30 @@ import static com.datasqrl.config.ConfigurationTest.testForErrors;
 
 import com.datasqrl.canonicalizer.Name;
 import com.datasqrl.error.ErrorCollector;
+import java.nio.file.Path;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ConfigurationSchemaTest {
 
+  private static final Path TEST_CASES = Path.of(CONFIG_DIR.toString(), "tableConfig");
 
   @Test
   public void readTableConfigsNormal() {
     ErrorCollector errors = ErrorCollector.root();
-    SqrlConfigCommons.fromFilesTableConfig(Name.system("MyTable"), errors, List.of(CONFIG_DIR.resolve("tableConfigFull.json")));
+    SqrlConfigCommons.fromFilesTableConfig(Name.system("MyTable"), errors, List.of(TEST_CASES.resolve("tableConfigFull.json")));
   }
 
   @Test
   public void readTableConfigsMissing() {
     testForErrors(errors -> SqrlConfigCommons.fromFilesTableConfig(Name.system("MyTable"), errors,
-        List.of(CONFIG_DIR.resolve("tableConfigMissing.json"))));
+        List.of(TEST_CASES.resolve("tableConfigMissing.json"))));
   }
 
   @Test
   public void readTableExtraKeys() {
     testForErrors(errors -> SqrlConfigCommons.fromFilesTableConfig(Name.system("MyTable"), errors,
-        List.of(CONFIG_DIR.resolve("tableConfigExtraField.json"))));
+        List.of(TEST_CASES.resolve("tableConfigExtraField.json"))));
   }
 
 }
