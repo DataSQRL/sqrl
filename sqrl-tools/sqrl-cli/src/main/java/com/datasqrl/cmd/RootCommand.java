9d2c4c4934bbda092b64cd504a5ae6169d85bbc0
Daniel Henneberger <git@danielhenneberger.com>
2024-09-27 16:54:21 -0700

    Prepare for 0.5.5
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-tools/sqrl-cli/src/main/java/com/datasqrl/cmd/RootCommand.java
+++ sqrl-tools/sqrl-cli/src/main/java/com/datasqrl/cmd/RootCommand.java
@@ -11,7 +11,7 @@ import lombok.NonNull;
 import picocli.CommandLine;
 import picocli.CommandLine.ScopeType;
 
-@CommandLine.Command(name = "datasqrl", mixinStandardHelpOptions = true, version = "v0.5.4",
+@CommandLine.Command(name = "datasqrl", mixinStandardHelpOptions = true, version = "v0.5.5",
     subcommands = {CompilerCommand.class,
          PublishCommand.class, TestCommand.class, LoginCommand.class, RunCommand.class})
 @Getter
