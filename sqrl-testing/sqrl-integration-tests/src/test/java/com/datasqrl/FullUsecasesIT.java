44c6b6343d81be5bb252a451f53b054a8233888a
Daniel Henneberger <git@danielhenneberger.com>
2024-10-01 16:56:00 -0700

    Disable duckdb
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/FullUsecasesIT.java
+++ sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/FullUsecasesIT.java
@@ -65,8 +65,8 @@ public class FullUsecasesIT {
       new ScriptCriteria("sensors-mutation.sqrl", "run"), //build server issues
       new ScriptCriteria("conference.sqrl", "test"), //minicluster has different results??
       new ScriptCriteria("conference.sqrl", "run"), //build server issues
-//      new ScriptCriteria("duckdb.sqrl", "test"), //fails in build server
-//      new ScriptCriteria("duckdb.sqrl", "run"), //fails in build server
+      new ScriptCriteria("duckdb.sqrl", "test"), //fails in build server
+      new ScriptCriteria("duckdb.sqrl", "run"), //fails in build server
       new ScriptCriteria("snowflake.sqrl", "test"), //fails in build server
       new ScriptCriteria("snowflake.sqrl", "run"), //fails in build server
       new ScriptCriteria("sensors-full.sqrl", "test"), //flaky
