011802ad7ec7fab3f818dc544e423252687ebda2
henneberger <git@danielhenneberger.com>
2024-10-05 18:22:58 -0700

    Add udf support to run/test (#851)
    * Add UDF support in run/test
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    * Add snaps
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    * Fix jar
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    * Add udf output
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    * Fix snaps
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>
    
    ---------
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/FullUsecasesIT.java
+++ sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/FullUsecasesIT.java
@@ -161,9 +161,9 @@ public class FullUsecasesIT {
 
       Map<String, String> env = new HashMap<>();
       env.putAll(System.getenv());
-      env.put("EXECUTION_MODE", "local");
       env.putAll(containerHook.getEnv());
       env.put("DATA_PATH", rootDir.resolve("build/deploy/flink/data").toAbsolutePath().toString());
+      env.put("UDF_PATH", rootDir.resolve("build/deploy/flink/lib").toAbsolutePath().toString());
 
       // Log test run
       log.info("The test parameters\n" +
@@ -237,7 +237,7 @@ public class FullUsecasesIT {
   @MethodSource("useCaseProvider")
   @Disabled
   public void runTestNumber(UseCaseTestParameter param) {
-    int i = -1;
+    int i = 31;
     testNo++;
     System.out.println(testNo + ":" + param);
     if (i == testNo) {
