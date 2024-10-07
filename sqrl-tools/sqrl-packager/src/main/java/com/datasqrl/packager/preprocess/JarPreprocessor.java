7a22347430b4dc00c1a216ff1e00a0601c75ee18
Nandor Soma Abonyi <nandor@datasqrl.com>
2024-10-07 09:13:15 +0200

    Consistent jar handling for Local and Remote UDFs (#849)
    * Consistent jar handling for Local and Remote UDFs
    
    * addressing review comments
    
    (cherry picked from commit 3f6c6230d3bec89442d63c4fbff523b8d228cda3)

--- sqrl-tools/sqrl-packager/src/main/java/com/datasqrl/packager/preprocess/JarPreprocessor.java
+++ sqrl-tools/sqrl-packager/src/main/java/com/datasqrl/packager/preprocess/JarPreprocessor.java
@@ -89,8 +89,11 @@ public class JarPreprocessor implements Preprocessor {
       processorContext.addLibrary(path);
     }
 
+    processorContext.addDependency(path);
+
     return null;
   }
+
   /**
    * Checks if the jar entry is valid
    */
