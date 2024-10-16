0a22dcf830e9f432f7867754ac749417959c4dc0
henneberger <git@danielhenneberger.com>
2024-10-16 09:31:41 -0700

    Fix null values for case insensitive datafetcher (#871) (#872)
    Fix null values for case insensitive datafetcher
    
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-server/sqrl-server-vertx/src/main/java/com/datasqrl/graphql/VertxContext.java
+++ sqrl-server/sqrl-server-vertx/src/main/java/com/datasqrl/graphql/VertxContext.java
@@ -4,50 +4,37 @@ import com.datasqrl.canonicalizer.NameCanonicalizer;
 import com.datasqrl.canonicalizer.ReservedName;
 import com.datasqrl.graphql.io.SinkConsumer;
 import com.datasqrl.graphql.io.SinkProducer;
-import com.datasqrl.graphql.server.GraphQLEngineBuilder;
 import com.datasqrl.graphql.server.Context;
+import com.datasqrl.graphql.server.GraphQLEngineBuilder;
 import com.datasqrl.graphql.server.JdbcClient;
+import com.datasqrl.graphql.server.QueryExecutionContext;
 import com.datasqrl.graphql.server.RootGraphqlModel.Argument;
-import com.datasqrl.graphql.server.RootGraphqlModel.JdbcParameterHandler;
 import com.datasqrl.graphql.server.RootGraphqlModel.KafkaMutationCoords;
-import com.datasqrl.graphql.server.RootGraphqlModel.MutationCoords;
 import com.datasqrl.graphql.server.RootGraphqlModel.MutationCoordsVisitor;
 import com.datasqrl.graphql.server.RootGraphqlModel.PostgresLogMutationCoords;
 import com.datasqrl.graphql.server.RootGraphqlModel.ResolvedQuery;
 import com.datasqrl.graphql.server.RootGraphqlModel.SubscriptionCoords;
 import com.datasqrl.graphql.server.RootGraphqlModel.VariableArgument;
-import com.datasqrl.graphql.server.QueryExecutionContext;
 import com.google.common.base.Preconditions;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.PropertyDataFetcher;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.handler.graphql.schema.VertxDataFetcher;
-import io.vertx.ext.web.handler.graphql.schema.VertxPropertyDataFetcher;
 import io.vertx.sqlclient.PreparedQuery;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.Tuple;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.Properties;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import lombok.SneakyThrows;
 import lombok.Value;
-import org.apache.calcite.jdbc.CalciteConnection;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -84,9 +71,10 @@ public class VertxContext implements Context {
             if (value != null) {
               return value;
             }
-            // bad hack, remove me
+            // Case-insensitive lookup for drivers that may not preserve sensitivity
             return jsonObject.getMap().entrySet().stream()
                 .filter(e->e.getKey().equalsIgnoreCase(getPropertyName()))
+                .filter(e -> e.getValue() != null)
                 .map(e->e.getValue())
                 .findAny()
                 .orElse(null);
@@ -96,6 +84,7 @@ public class VertxContext implements Context {
       };
     }
   }
+
   @Override
   public DataFetcher<?> createArgumentLookupFetcher(GraphQLEngineBuilder server, Map<Set<Argument>, ResolvedQuery> lookupMap) {
     return VertxDataFetcher.create((env, fut) -> {
