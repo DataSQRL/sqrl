4a31e114e85cbc9660a474ead60f6b79d607a8c2
Daniel Henneberger <git@danielhenneberger.com>
2024-10-27 09:14:18 -0700

    Align closer to the flink function catalog
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-calcite/src/main/java/com/datasqrl/calcite/OperatorTable.java
+++ sqrl-calcite/src/main/java/com/datasqrl/calcite/OperatorTable.java
@@ -12,6 +12,7 @@ import org.apache.calcite.sql.SqlOperatorTable;
 import org.apache.calcite.sql.SqlSyntax;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.validate.SqlNameMatcher;
+import org.apache.flink.table.planner.functions.sql.FlinkSqlOperatorTable;
 
 public class OperatorTable implements SqlOperatorTable {
 
@@ -20,31 +21,17 @@ public class OperatorTable implements SqlOperatorTable {
 
   @Inject
   public OperatorTable(CatalogReader catalogReader, SqrlSchema schema) {
-    this.chain = new SqlOperatorTable[]{catalogReader, SqlStdOperatorTable.instance()};
+    this.chain = new SqlOperatorTable[]{catalogReader, FlinkSqlOperatorTable.instance(false)};
     this.schema = schema;
   }
 
   @Override
   public void lookupOperatorOverloads(SqlIdentifier sqlIdentifier, SqlFunctionCategory sqlFunctionCategory, SqlSyntax sqlSyntax, List<SqlOperator> list, SqlNameMatcher sqlNameMatcher) {
-    if (list.isEmpty()) {
-      SqlOperator fn = sqlNameMatcher.get(schema.getUdfListMap(), List.of(), List.of(sqlIdentifier.getSimple()));
-      if (fn != null) {
-        list.add(fn);
-      }
+    SqlOperator fn = sqlNameMatcher.get(schema.getUdfListMap(), List.of(), List.of(sqlIdentifier.getSimple()));
+    if (fn != null) {
+      list.add(fn);
     }
 
-    //Also check the function name since calcite will convert to their function name
-    if (list.isEmpty()) {
-      SqlOperator fn = sqlNameMatcher.get(schema.getInternalNames(), List.of(), List.of(sqlIdentifier.getSimple()));
-      if (fn != null) {
-        list.add(fn);
-      }
-    }
-
-    //Exit early so we don't get duplicate calcite functions
-    if (!list.isEmpty()) {
-      return;
-    }
     for (SqlOperatorTable table : chain) {
       table.lookupOperatorOverloads(sqlIdentifier, sqlFunctionCategory, sqlSyntax, list, sqlNameMatcher);
     }
