8f9f33b7e2a71a1843725363ecdc99891f2a7bb5
Daniel Henneberger <git@danielhenneberger.com>
2024-11-04 18:22:33 -0800

    Fix #965 by using the legacy timestamp mapping for avro schemas
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-planner/src/main/java/com/datasqrl/io/schema/avro/AvroSchemaToRelDataTypeFactory.java
+++ sqrl-planner/src/main/java/com/datasqrl/io/schema/avro/AvroSchemaToRelDataTypeFactory.java
@@ -36,6 +36,15 @@ public class AvroSchemaToRelDataTypeFactory implements SchemaToRelDataTypeFactor
   private boolean getLegacyTimestampMapping(TableConfig tableConfig) {
     if (tableConfig == null) return true;
     Map<String, Object> configMap = tableConfig.getConnectorConfig().toMap();
+    //Todo update this to extract avro format specifically from other connectors
+    Object format = configMap.get("format");
+    if (format == null) {
+      format = configMap.get("value.format");
+    }
+    if (format instanceof String && !((String) format).equalsIgnoreCase("avro")) {
+      return false;
+    }
+
     Object legacy = configMap.get("avro.timestamp_mapping.legacy");
     if (legacy instanceof Boolean) {
       return (Boolean) legacy;
