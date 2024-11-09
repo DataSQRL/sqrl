3ef014990b43854f4c4ae2ce4860f6cfde19dee8
Daniel Henneberger <git@danielhenneberger.com>
2024-11-09 08:00:13 -0800

    Fix serialize to bytes
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- sqrl-planner/src/main/java/com/datasqrl/datatype/flink/json/FlexibleJsonFlinkFormatTypeMapper.java
+++ sqrl-planner/src/main/java/com/datasqrl/datatype/flink/json/FlexibleJsonFlinkFormatTypeMapper.java
@@ -91,7 +91,7 @@ public class FlexibleJsonFlinkFormatTypeMapper extends FlinkDataTypeMapper {
 
     // Cast needed, convert to bytes
     return Optional.of(
-        new CastFunction(SerializeToBytes.class.getSimpleName(),
+        new CastFunction(SerializeToBytes.class.getName(),
             convert(new SerializeToBytes())));
   }
 
