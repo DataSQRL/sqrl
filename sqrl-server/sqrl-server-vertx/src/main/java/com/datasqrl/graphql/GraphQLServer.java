996302a0bfc7c2e3b03cdca62123a71c45322317
Marvin Froeder <marvin@datasqrl.com>
2025-02-03 13:11:14 -0300

    Add missing java.util.List
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

--- sqrl-server/sqrl-server-vertx/src/main/java/com/datasqrl/graphql/GraphQLServer.java
+++ sqrl-server/sqrl-server-vertx/src/main/java/com/datasqrl/graphql/GraphQLServer.java
@@ -45,6 +45,7 @@ import io.vertx.pgclient.impl.PgPoolOptions;
 import io.vertx.sqlclient.SqlClient;
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
