ac4dd35681e1b132dea77dec95cfbb7d5ac36e09
Etienne Chauchot <echauchot@apache.org>
2025-03-05 16:08:48 +0100

    Implement subscriptions in GraphqlModelGenerator2

--- sqrl-planner/src/main/java/com/datasqrl/v2/graphql/GraphqlSchemaWalker2.java
+++ sqrl-planner/src/main/java/com/datasqrl/v2/graphql/GraphqlSchemaWalker2.java
@@ -8,6 +8,7 @@ import com.datasqrl.canonicalizer.Name;
 import com.datasqrl.canonicalizer.NamePath;
 import com.datasqrl.graphql.APIConnectorManager;
 import com.datasqrl.plan.queries.APISource;
+import com.datasqrl.v2.APIConnectorManager2;
 import com.datasqrl.v2.parser.AccessModifier;
 import com.datasqrl.v2.tables.SqrlTableFunction;
 import graphql.language.FieldDefinition;
@@ -37,9 +38,9 @@ public abstract class GraphqlSchemaWalker2 {
 
 //  protected final SqlNameMatcher nameMatcher;
   protected final List<SqrlTableFunction> tableFunctions;
-  protected final APIConnectorManager apiManager;
+  protected final APIConnectorManager2 apiManager;
 
-  public GraphqlSchemaWalker2(List<SqrlTableFunction> tableFunctions, APIConnectorManager apiManager) {
+  public GraphqlSchemaWalker2(List<SqrlTableFunction> tableFunctions, APIConnectorManager2 apiManager) {
     this.tableFunctions = tableFunctions;
     this.apiManager = apiManager;
   }
@@ -91,7 +92,7 @@ public abstract class GraphqlSchemaWalker2 {
       if (tableFunction.getVisibility().getAccess() == AccessModifier.QUERY) { // walking a query table function
         visitQuery(objectType, atField, tableFunction);
       } else { // walking a subscription table function
-        visitSubscription(objectType, atField, registry, apiSource);
+        visitSubscription(objectType, atField, tableFunction, registry, apiSource);
       }
       RelDataType functionRowType = tableFunction.getRowType();
       walkObjectType(true, objectType, functionPath, Optional.of(functionRowType), registry, apiSource);
@@ -172,7 +173,7 @@ public abstract class GraphqlSchemaWalker2 {
    */
   protected abstract void visitQuery(ObjectTypeDefinition resultType, FieldDefinition field, SqrlTableFunction tableFunction);
 
-  protected abstract void visitSubscription(ObjectTypeDefinition objectType, FieldDefinition field,
+  protected abstract void visitSubscription(ObjectTypeDefinition objectType, FieldDefinition field, SqrlTableFunction tableFunction,
                                             TypeDefinitionRegistry registry, APISource source);
 
   protected abstract void visitMutation(ObjectTypeDefinition objectType, FieldDefinition field, TypeDefinitionRegistry registry,
