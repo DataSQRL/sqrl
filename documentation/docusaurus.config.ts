590d6717dc0f3a069f7f4c5a8ecd79b0812ac588
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2025-05-13 13:57:57 -0700

    Move to docs.datasqrl.com (#1203)
    move to docs.datasqrl.com
    
    Signed-off-by: Matthias Broecheler <mbroecheler@users.noreply.github.com>

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://datasqrl.github.io/',
+  url: 'https://docs.datasqrl.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sqrl/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
