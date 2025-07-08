805f8a1fbddf0df9ef94748a15e25d52c7b78a82
Marvin Froeder <marvin@datasqrl.com>
2025-07-08 07:34:41 -0300

    Create test for sqrl test command
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

---
+++ sqrl-testing/sqrl-container-testing/src/test/java/com/datasqrl/container/testing/SqrlTestContainerIT.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2021 DataSQRL (contact@datasqrl.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datasqrl.container.testing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class SqrlTestContainerIT extends SqrlContainerTestBase {
+
+  @Override
+  protected String getTestCaseName() {
+    return "avro-schema";
+  }
+
+  @Test
+  @SneakyThrows
+  void givenAvroSchemaScript_whenTestCommandExecuted_thenSnapshotsValidateSuccessfully() {
+    var result =
+        sqrlScript(
+            testDir,
+            "test avro-schema.sqrl --snapshots snapshots-avro-schema --tests tests-avro-schema"
+                .split(" "));
+
+    log.info("SQRL test command executed successfully");
+    log.info("Container logs:\n{}", result.logs());
+
+    // Verify the expected success messages are present in the logs
+    assertThat(result.logs())
+        .contains("Snapshot OK for MySchemaQuery")
+        .contains("Snapshot OK for MySchema");
+
+    log.info("All snapshot validations passed successfully");
+  }
+}
