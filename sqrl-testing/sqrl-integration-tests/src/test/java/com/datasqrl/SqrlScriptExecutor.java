d8730cd0a4294a33830ae8a6d51af12c7f66c736
Ferenc Csaky <ferenc@datasqrl.com>
2025-07-24 18:24:14 +0200

    Simplify FullUseCasesIT and the relevant test execution parts

--- sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/SqrlScriptExecutor.java
+++ sqrl-testing/sqrl-integration-tests/src/test/java/com/datasqrl/SqrlScriptExecutor.java
@@ -15,14 +15,12 @@
  */
 package com.datasqrl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 import com.datasqrl.cli.AssertStatusHook;
 import com.datasqrl.cli.DatasqrlCli;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -30,32 +28,19 @@ import lombok.Getter;
 @Builder
 public class SqrlScriptExecutor {
 
-  private Path rootDir;
+  private Path packageJsonPath;
   private String goal;
-  private String script;
-  private String graphql;
-  private String testSuffix;
-  private String testPath;
-  private String packageJsonPath;
-  private List<String> additionalArgs;
 
   public void execute(AssertStatusHook hook) {
-    List<String> argsList = new ArrayList<>();
-    argsList.add(goal);
-    argsList.add(script);
-    if (graphql != null && !graphql.isEmpty()) {
-      argsList.add(graphql);
-    }
-    if (additionalArgs != null && !additionalArgs.isEmpty()) {
-      argsList.addAll(additionalArgs);
-    }
-    if (getPackageJsonPath() != null) {
-      argsList.addAll(Arrays.asList("-c", getPackageJsonPath()));
-    }
+    assertThat(packageJsonPath).as("Package JSON must be provided").exists();
+    var rootDir = packageJsonPath.getParent();
+    var fileName = packageJsonPath.getFileName().toString();
+
     // Execute the command
     var cli = new DatasqrlCli(rootDir, hook, true);
-    var exitCode =
-        cli.getCmd().execute(argsList.toArray(new String[0])) + (hook.isSuccess() ? 0 : 1);
+    var exitCode = cli.getCmd().execute(goal, "-c", fileName);
+    exitCode += hook.isSuccess() ? 0 : 1;
+
     if (exitCode != 0) {
       if (hook.failure() != null) {
         fail("", hook.failure());
