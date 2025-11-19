359f1ccc03e6984014b30589f0ef4dea27af543f
Marvin Froeder <marvin@datasqrl.com>
2025-11-19 17:31:23 -0300

    Implement BIN, INSTR, and STARTSWITH function translations for PostgreSQL
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

---
+++ sqrl-planner/src/main/java/com/datasqrl/function/translation/postgres/builtinflink/StartsWithSqlTranslation.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2021 DataSQRL (contact@datasqrl.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datasqrl.function.translation.postgres.builtinflink;
+
+import com.datasqrl.function.CalciteFunctionUtil;
+import com.datasqrl.function.translation.PostgresSqlTranslation;
+import com.datasqrl.function.translation.SqlTranslation;
+import com.google.auto.service.AutoService;
+import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlWriter;
+import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.flink.table.functions.BuiltInFunctionDefinitions;
+
+@AutoService(SqlTranslation.class)
+public class StartsWithSqlTranslation extends PostgresSqlTranslation {
+
+  public StartsWithSqlTranslation() {
+    super(CalciteFunctionUtil.lightweightOp(BuiltInFunctionDefinitions.STARTS_WITH));
+  }
+
+  @Override
+  public void unparse(SqlCall call, SqlWriter writer, int leftPrec, int rightPrec) {
+    var expr = call.getOperandList().get(0);
+    var prefix = call.getOperandList().get(1);
+
+    CalciteFunctionUtil.lightweightOp("starts_with")
+        .createCall(SqlParserPos.ZERO, expr, prefix)
+        .unparse(writer, leftPrec, rightPrec);
+  }
+}
