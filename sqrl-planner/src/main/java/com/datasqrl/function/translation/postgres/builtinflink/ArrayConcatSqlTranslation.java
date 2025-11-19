686bc4c07ca8ce205b03c492866327fe9021abc5
Marvin Froeder <marvin@datasqrl.com>
2025-11-19 17:48:45 -0300

    implement COLLECT, LISTAGG, ARRAY_CONCAT, MAP_UNION, and CARDINALITY function translations
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

---
+++ sqrl-planner/src/main/java/com/datasqrl/function/translation/postgres/builtinflink/ArrayConcatSqlTranslation.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2021 DataSQRL (contact@datasqrl.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datasqrl.function.translation.postgres.builtinflink;
+
+import com.datasqrl.function.CalciteFunctionUtil;
+import com.datasqrl.function.translation.PostgresSqlTranslation;
+import com.datasqrl.function.translation.SqlTranslation;
+import com.google.auto.service.AutoService;
+import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlWriter;
+import org.apache.calcite.sql.parser.SqlParserPos;
+
+@AutoService(SqlTranslation.class)
+public class ArrayConcatSqlTranslation extends PostgresSqlTranslation {
+
+  public ArrayConcatSqlTranslation() {
+    super(CalciteFunctionUtil.lightweightOp("ARRAY_CONCAT"));
+  }
+
+  @Override
+  public void unparse(SqlCall call, SqlWriter writer, int leftPrec, int rightPrec) {
+    var operands = call.getOperandList();
+    if (operands.isEmpty()) {
+      return;
+    }
+
+    SqlNode result = operands.get(0);
+    for (int i = 1; i < operands.size(); i++) {
+      result =
+          CalciteFunctionUtil.lightweightOp("array_cat")
+              .createCall(SqlParserPos.ZERO, result, operands.get(i));
+    }
+
+    result.unparse(writer, leftPrec, rightPrec);
+  }
+}
