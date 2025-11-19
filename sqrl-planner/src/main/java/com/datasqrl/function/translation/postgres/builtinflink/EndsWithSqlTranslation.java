b31632e0bbd3ab8a8e601667dde4baa99281f7cb
Marvin Froeder <marvin@datasqrl.com>
2025-11-19 17:40:04 -0300

    Implement ENDSWITH, REGEXP, and SPLIT_INDEX function translations
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

---
+++ sqrl-planner/src/main/java/com/datasqrl/function/translation/postgres/builtinflink/EndsWithSqlTranslation.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2021 DataSQRL (contact@datasqrl.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datasqrl.function.translation.postgres.builtinflink;
+
+import com.datasqrl.function.CalciteFunctionUtil;
+import com.datasqrl.function.translation.PostgresSqlTranslation;
+import com.datasqrl.function.translation.SqlTranslation;
+import com.google.auto.service.AutoService;
+import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlWriter;
+import org.apache.calcite.sql.fun.SqlStdOperatorTable;
+import org.apache.calcite.sql.parser.SqlParserPos;
+
+@AutoService(SqlTranslation.class)
+public class EndsWithSqlTranslation extends PostgresSqlTranslation {
+
+  public EndsWithSqlTranslation() {
+    super(CalciteFunctionUtil.lightweightOp("ENDSWITH"));
+  }
+
+  @Override
+  public void unparse(SqlCall call, SqlWriter writer, int leftPrec, int rightPrec) {
+    var expr = call.getOperandList().get(0);
+    var suffix = call.getOperandList().get(1);
+
+    var charLengthCall =
+        CalciteFunctionUtil.lightweightOp("char_length").createCall(SqlParserPos.ZERO, suffix);
+
+    var rightCall =
+        CalciteFunctionUtil.lightweightOp("right")
+            .createCall(SqlParserPos.ZERO, expr, charLengthCall);
+
+    SqlStdOperatorTable.EQUALS
+        .createCall(SqlParserPos.ZERO, rightCall, suffix)
+        .unparse(writer, leftPrec, rightPrec);
+  }
+}
