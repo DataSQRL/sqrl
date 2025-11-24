13f0503050b0bf918768fdc3f2477825cda3ead5
Ferenc Csaky <ferenc@datasqrl.com>
2025-11-24 13:46:37 +0100

    Add `ARRAY_AGG` test case

--- sqrl-testing/sqrl-testing-integration/src/test/resources/usecases/filtered-distinct/ecommerce/orders.table.sql
+++ sqrl-testing/sqrl-testing-integration/src/test/resources/usecases/filtered-distinct/ecommerce/orders.table.sql
@@ -2,9 +2,5 @@ CREATE TABLE Orders (
      `_ingest_time` AS PROCTIME(),
      PRIMARY KEY (id, customerid, `time`) NOT ENFORCED
 ) WITH (
-      'format' = 'flexible-json',
-      'path' = '${DATA_PATH}/orders.jsonl',
-      'source.monitor-interval' = '10000',
-      'connector' = 'filesystem'
-      )
-LIKE `orders.schema.yml`;
+      'source.monitor-interval' = '10000'
+) LIKE `orders.jsonl`;
