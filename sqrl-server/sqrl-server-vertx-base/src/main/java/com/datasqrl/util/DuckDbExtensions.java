87e0b812a92f2d969fe3529e240e655a7ac989c7
Ferenc Csaky <ferenc@datasqrl.com>
2026-01-06 13:47:16 +0100

    Preinstall DuckDB extensions, and only load them at runtime

--- sqrl-server/sqrl-server-vertx-base/src/main/java/com/datasqrl/util/DuckDbExtensions.java
+++ sqrl-server/sqrl-server-vertx-base/src/main/java/com/datasqrl/util/DuckDbExtensions.java
@@ -15,36 +15,42 @@
  */
 package com.datasqrl.util;
 
+import static com.datasqrl.env.EnvVariableNames.DUCKDB_EXTENSIONS_DIR;
+
 import com.datasqrl.graphql.config.JdbcConfig;
+import java.util.Optional;
 import java.util.StringJoiner;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 public final class DuckDbExtensions {
 
   private final StringJoiner joiner = new StringJoiner(";", "", ";");
 
   private final JdbcConfig.DuckDbConfig config;
 
-  public String buildInitSql() {
-    joiner.add("INSTALL iceberg");
-    joiner.add("INSTALL httpfs");
-    ifDiskCache("INSTALL cache_httpfs FROM community");
+  public Optional<String> buildInitSql() {
+    var extensionDir = System.getenv(DUCKDB_EXTENSIONS_DIR);
 
+    if (extensionDir == null || extensionDir.trim().isEmpty()) {
+      log.warn("Environment variable {} is not set, extensions will not be loaded.", extensionDir);
+      return Optional.empty();
+    }
+
+    joiner.add("SET extension_directory='" + extensionDir + "'");
     joiner.add("LOAD iceberg");
     joiner.add("LOAD httpfs");
-    ifDiskCache("LOAD cache_httpfs");
+
+    if (config.isUseDiskCache()) {
+      joiner.add("LOAD cache_httpfs");
+    }
 
     if (config.isUseVersionGuessing()) {
       joiner.add("SET unsafe_enable_version_guessing = true");
     }
 
-    return joiner.toString();
-  }
-
-  private void ifDiskCache(String stmt) {
-    if (config.isUseDiskCache()) {
-      joiner.add(stmt);
-    }
+    return Optional.of(joiner.toString());
   }
 }
