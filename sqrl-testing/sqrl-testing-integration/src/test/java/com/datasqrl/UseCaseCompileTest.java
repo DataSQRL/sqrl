fb39387489b09aea5b091ba7e5ac3c3ba6c9072b
Ferenc Csaky <ferenc@datasqrl.com>
2026-04-15 16:15:34 +0200

    feat: Add remaining REGEXP relation DuckDB function translations (#2015)

--- sqrl-testing/sqrl-testing-integration/src/test/java/com/datasqrl/UseCaseCompileTest.java
+++ sqrl-testing/sqrl-testing-integration/src/test/java/com/datasqrl/UseCaseCompileTest.java
@@ -55,7 +55,7 @@ public class UseCaseCompileTest {
   @Test
   @Disabled("Intended for manual usage")
   void runTestCaseByName() {
-    var pkg = USECASE_DIR.resolve("multi-batch-compile").resolve("package.json");
+    var pkg = USECASE_DIR.resolve("function-translation/duckdb").resolve("package.json");
     UseCaseTestHelper.testUseCase(
         snapshotExtension,
         getClass(),
