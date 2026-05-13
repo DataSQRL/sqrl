c031aca669931e7f1560c182000eaae1b4f06f69
Ferenc Csaky <ferenc@datasqrl.com>
2026-05-13 22:21:02 +0200

    fix: Rework comment parsing in `SqlScriptStatementSplitter`

--- sqrl-planner/src/test/java/com/datasqrl/planner/parser/SqlScriptStatementSplitterTest.java
+++ sqrl-planner/src/test/java/com/datasqrl/planner/parser/SqlScriptStatementSplitterTest.java
@@ -41,6 +41,31 @@ class SqlScriptStatementSplitterTest {
   }
 
   @Test
+  void givenDocCommentMarkerInStringLiteral_whenSplitStatements_thenPreservesLiteral() {
+    var script =
+        """
+        SELECT 'first line;
+                /** doc comment
+                    multi line
+                */
+                last line' AS `val`
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            SELECT 'first line;
+                    /** doc comment
+                        multi line
+                    */
+                    last line' AS `val`;
+            """);
+  }
+
+  @Test
   void givenLineCommentOutsideStringLiteral_whenSplitStatements_thenRemovesComment() {
     var script =
         """
@@ -75,6 +100,144 @@ class SqlScriptStatementSplitterTest {
             SELECT 'first line;
                     -- not a comment
                     last line' AS `val`;
+            """);
+  }
+
+  @Test
+  void givenSingleQuoteInSingleLineDocComment_whenSplitStatements_thenIgnoresQuote() {
+    var script =
+        """
+        /** Dummy one liner comment. '' This -- is still part of the doc' comment. */
+        SELECT 'hello -- not a comment' AS `val`;-- comment
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            /** Dummy one liner comment. '' This -- is still part of the doc' comment. */
+            SELECT 'hello -- not a comment' AS `val`;
+            """);
+  }
+
+  @Test
+  void givenSingleQuoteInMultiLineDocComment_whenSplitStatements_thenIgnoresQuote() {
+    var script =
+        """
+        /** Dummy multi line comment. ''
+            This -- is still part of the doc' comment. */
+        SELECT 'hello -- not a comment' AS `val`;-- comment
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            /** Dummy multi line comment. ''
+                This -- is still part of the doc' comment. */
+            SELECT 'hello -- not a comment' AS `val`;
+            """);
+  }
+
+  @Test
+  void givenBlockCommentInStringLiteral_whenSplitStatements_thenPreservesLiteral() {
+    var script =
+        """
+        SELECT 'first line;
+                /* block comment
+                    multi line
+                */
+                last line' AS `val`
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            SELECT 'first line;
+                    /* block comment
+                        multi line
+                    */
+                    last line' AS `val`;
+            """);
+  }
+
+  @Test
+  void givenBlockCommentOutsideStringLiteral_whenSplitStatements_thenRemovesComment() {
+    var script =
+        """
+        SELECT 1 /* regular block comment */ AS `val`
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            SELECT 1  AS `val`;
+            """);
+  }
+
+  @Test
+  void givenMultilineBlockCommentOutsideStringLiteral_whenSplitStatements_thenRemovesComment() {
+    var script =
+        """
+        SELECT 1 AS `before`
+        /* block comment with ' quote, -- line comment, and ; delimiter
+           still inside the block comment
+        */
+        , 2 AS `after`
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            SELECT 1 AS `before`
+            , 2 AS `after`;
+            """);
+  }
+
+  @Test
+  void givenSqlHintComment_whenSplitStatements_thenPreservesHint() {
+    var script =
+        """
+        SELECT /*+ OPTIONS('key'='value') */ 1 AS `val`
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            SELECT /*+ OPTIONS('key'='value') */ 1 AS `val`;
+            """);
+  }
+
+  @Test
+  void givenWhitespaceBeforeHintPlus_whenSplitStatements_thenRemovesBlockComment() {
+    var script =
+        """
+        SELECT /* + not a hint */ 1 AS `val`
+        """;
+
+    var statements = SqlScriptStatementSplitter.splitStatements(script);
+
+    assertThat(statements)
+        .extracting(ParsedObject::get)
+        .containsExactly(
+            """
+            SELECT  1 AS `val`;
             """);
   }
 }
