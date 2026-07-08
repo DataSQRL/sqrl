2734950974d8cbad91948c8926ad65363b869dd8
Ferenc Csaky <ferenc@datasqrl.com>
2026-07-08 16:07:07 +0200

    fix: Disable Iceberg source parallelism inference locally
    # Conflicts:
    #	sqrl-planner/src/test/java/com/datasqrl/planner/FlinkPhysicalPlanTest.java

--- sqrl-planner/src/test/java/com/datasqrl/planner/FlinkPhysicalPlanTest.java
+++ sqrl-planner/src/test/java/com/datasqrl/planner/FlinkPhysicalPlanTest.java
@@ -15,20 +15,49 @@
  */
 package com.datasqrl.planner;
 
+import static com.datasqrl.planner.FlinkPhysicalPlan.ICEBERG_INFER_SOURCE_PARALLELISM;
+import static com.datasqrl.planner.FlinkPhysicalPlan.ICEBERG_USE_V2_SINK;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.datasqrl.engine.database.relational.IcebergEngineFactory;
 import com.datasqrl.engine.stream.flink.FlinkSqlNodes;
 import com.datasqrl.planner.tables.FlinkConnectorConfigWrapper;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.apache.calcite.rel.type.RelDataTypeSystem;
 import org.apache.calcite.sql.SqlNodeList;
 import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.sql.type.SqlTypeFactoryImpl;
+import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.sql.parser.ddl.table.SqlCreateTable;
 import org.junit.jupiter.api.Test;
 
 class FlinkPhysicalPlanTest {
+
+  @Test
+  void givenIcebergConnector_whenBuild_thenAppliesIcebergConfigAdjustments() {
+    var builder = new FlinkPhysicalPlan.Builder(new Configuration());
+    builder.add(createTable(IcebergEngineFactory.ENGINE_NAME));
+
+    var plan = builder.build(Optional.empty());
+
+    assertThat(plan.getConfig().getString(ICEBERG_USE_V2_SINK, null)).isEqualTo("true");
+    assertThat(plan.getConfig().getString(ICEBERG_INFER_SOURCE_PARALLELISM, null))
+        .isEqualTo("false");
+  }
+
+  @Test
+  void givenNonIcebergConnector_whenBuild_thenDoesNotApplyIcebergConfigAdjustments() {
+    var builder = new FlinkPhysicalPlan.Builder(new Configuration());
+    builder.add(createTable("filesystem"));
+
+    var plan = builder.build(Optional.empty());
+
+    assertThat(plan.getConfig().getString(ICEBERG_USE_V2_SINK, null)).isNull();
+    assertThat(plan.getConfig().getString(ICEBERG_INFER_SOURCE_PARALLELISM, null)).isNull();
+  }
 
   @Test
   void givenCreateTableWithConnectorAndFormats_whenBuild_thenClassifiesPropertiesSeparately() {
@@ -57,5 +86,13 @@ class FlinkPhysicalPlanTest {
 
     assertThat(plan.getConnectors()).containsExactly("kafka");
     assertThat(plan.getFormats()).containsExactlyInAnyOrder("json", "avro");
+  }
+
+  private SqlCreateTable createTable(String connector) {
+    var typeFactory = new SqlTypeFactoryImpl(RelDataTypeSystem.DEFAULT);
+    var rowType =
+        typeFactory.builder().add("id", typeFactory.createSqlType(SqlTypeName.INTEGER)).build();
+
+    return FlinkSqlNodes.createTable("test_table", rowType, Map.of("connector", connector), false);
   }
 }
