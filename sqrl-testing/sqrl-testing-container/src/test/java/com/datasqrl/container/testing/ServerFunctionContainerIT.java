6e414e8cc3ed4513d894ac4ed77002b7798deecb
Ferenc Csaky <ferenc@datasqrl.com>
2026-08-05 13:10:44 +0200

    test: Re-enable tests that use the OpenAI API

--- sqrl-testing/sqrl-testing-container/src/test/java/com/datasqrl/container/testing/ServerFunctionContainerIT.java
+++ sqrl-testing/sqrl-testing-container/src/test/java/com/datasqrl/container/testing/ServerFunctionContainerIT.java
@@ -21,11 +21,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import lombok.SneakyThrows;
 import org.apache.http.util.EntityUtils;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-@Disabled
 public class ServerFunctionContainerIT {
 
   @RegisterExtension
