86b1d9f4f78db9ceb657216e89e27a3298ffcfa3
Ferenc Csaky <ferenc@datasqrl.com>
2026-08-21 16:01:09 +0200

    test: Add `LATERAL JOIN` table function test case (#2295)

---
+++ sqrl-testing/sqrl-testing-integration/src/test/java/com/datasqrl/functions/DuplicateStringFunction.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2021 DataSQRL (contact@datasqrl.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datasqrl.functions;
+
+import com.google.auto.service.AutoService;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.catalog.DataTypeFactory;
+import org.apache.flink.table.functions.TableFunction;
+import org.apache.flink.table.types.inference.InputTypeStrategies;
+import org.apache.flink.table.types.inference.TypeInference;
+import org.apache.flink.table.types.inference.TypeStrategies;
+
+@AutoService(TableFunction.class)
+public class DuplicateStringFunction extends TableFunction<String> {
+
+  public void eval(Object... values) {
+    var value = String.valueOf(values[0]);
+    collect(value);
+    collect(value);
+  }
+
+  @Override
+  public TypeInference getTypeInference(DataTypeFactory typeFactory) {
+    var inputTypeStrategy =
+        InputTypeStrategies.compositeSequence().finishWithVarying(InputTypeStrategies.WILDCARD);
+
+    return TypeInference.newBuilder()
+        .inputTypeStrategy(inputTypeStrategy)
+        .outputTypeStrategy(TypeStrategies.explicit(DataTypes.STRING()))
+        .build();
+  }
+}
