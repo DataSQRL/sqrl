9d99bb085d947cae638397c588ec50369113e8fa
Ferenc Csaky <ferenc@datasqrl.com>
2026-09-23 11:00:15 +0200

    fix: Correct table type for window deduplication (#2391)

--- sqrl-testing/sqrl-testing-integration/src/test/java/com/datasqrl/DAGPlannerTest.java
+++ sqrl-testing/sqrl-testing-integration/src/test/java/com/datasqrl/DAGPlannerTest.java
@@ -78,7 +78,7 @@ public class DAGPlannerTest {
   @Disabled
   @Test
   void specificScript() {
-    var script = SCRIPT_DIR.resolve("insertIntoNow.sqrl");
+    var script = SCRIPT_DIR.resolve("windowDeduplication.sqrl");
     scripts(script);
   }
 
